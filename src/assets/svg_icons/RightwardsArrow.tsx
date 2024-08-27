3d1da8ef0a8ff53b4a1f6292d30cef710fbd4de9
Aliona-D <alona.danyliuk@inventorsoft.co>
2024-08-27 14:05:43 +0300

    Add images and svg-icons in assets

---
+++ src/assets/svg_icons/RightwardsArrow.tsx
@@ -0,0 +1,14 @@
+const RightwardsArrow = () => (
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2.66797 8H13.3346" stroke="#FAA6FF" stroke-width="0.666667" stroke-linecap="round" stroke-linejoin="round" />
+    <path
+      d="M8.66797 3.33203L13.3346 7.9987L8.66797 12.6654"
+      stroke="#FAA6FF"
+      stroke-width="0.666667"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+)
+
+export default RightwardsArrow
