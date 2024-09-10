9db17b986b05d8126c6e5521e32f8b2c07970027
Aliona-D <alona.danyliuk@inventorsoft.co>
2024-09-10 15:21:35 +0300

    Install styled-components, add ThemeProvider with colors and override radix default themes

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import { Theme as DefaultRadixTheme } from "@radix-ui/themes";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Theme accentColor="green">
-        <App />
-      </Theme>
+      <ThemeProvider theme={theme}>
+        <DefaultRadixTheme>
+          <App />
+        </DefaultRadixTheme>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/theme/theme.ts
@@ -0,0 +1,21 @@
+export const theme = {
+  background: '#2C2C2C',
+  white: '#FFFFFF',
+  black: '#000000',
+  green1: '#5FD0AB',
+  green2: '#50FFB1',
+  red: '#FF0000',
+  red1: '#FF648A',
+  red2: '#FF8080',
+  darkBlue: '#39456B',
+  blue1: '#E5F6FF',
+  blue2: '#71CEFF',
+  blue3: '#12B4FF',
+  grey1: '#F2F2F2',
+  grey2: '#ECECEC',
+  grey3: '#A8A6A6',
+  grey4: '#393939',
+  grey5: '#373A44',
+  grey6: '#2E3343',
+  grey7: '#111111'
+};
