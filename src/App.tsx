823f04152283445bdcb75f4045f6d2a90289559c
Aliona-D <alona.danyliuk@inventorsoft.co>
2024-09-27 22:01:41 +0300

    Add charting library

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,54 @@ import WalletChainBox from "./components/WalletChainBox/WalletChainBox";
 import Markets from "./pages/Markets/Markets";
 import { theme } from "./theme/theme";
 import useSDK from "./hooks/useSDK";
+import { useEffect, useState } from "react";
+
+const LEADERBOARD_POINTS_API =
+  "https://api.overlay.market/point-system/points/leaderboard/4";
 
 const App = () => {
-  useSDK()
+  const [markets, setMarkets] = useState<any>([]);
+
+  const sdk = useSDK();
+  console.log({ sdk });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const activeMarkets = await sdk.markets.getActiveMarkets();
+        const leaderboard = await sdk.markets.getLeaderboard();
+        // console.log("ETH Dominance", await sdk.markets.getMarketDetails("NodeMonkes"));
+        // console.log("activeMarkets", activeMarkets);
+        console.log({ leaderboard });
+        // activeMarkets && setMarkets(activeMarkets);
+      } catch (error) {
+        console.error("Error fetching markets:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log({ markets });
+
+  const [pointsData, setPointsData] = useState(null);
+
+  const getPointsData = async () => {
+    try {
+      const response = await fetch(LEADERBOARD_POINTS_API);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setPointsData(data);
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    getPointsData();
+  }, []);
+
+  console.log({ pointsData });
+
   return (
     <Container
       maxWidth={"1280px"}
