e18a072c5e9f303ad3aa2b2842d5ff6c2761e960
Aliona-D <alona.danyliuk@inventorsoft.co>
2024-10-02 11:12:58 +0300

    Define imola chain

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,3 +1,5 @@
+import { defineChain } from 'viem'
+
 export enum SUPPORTED_CHAINID {
   MAINNET = 1, //at launch
   RINKEBY = 4, //pre-launch only
@@ -8,3 +10,21 @@ export enum SUPPORTED_CHAINID {
   IMOLA = 30732,
   BARTIO = 80084,
 }
+ 
+export const imola = defineChain({
+  id: 30732,
+  name: 'Movement',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'MOVE',
+    symbol: 'MOVE',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://overlay-rpc.devnet.imola.movementnetwork.xyz'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://explorer.devnet.imola.movementlabs.xyz/#/?network=testnet' },
+  }
+})
