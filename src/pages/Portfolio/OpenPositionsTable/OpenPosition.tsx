0c8c625259a57a868b90b005b6f5ff6e515229e3
Aliona-D <alona.danyliuk@inventorsoft.co>
2024-10-30 16:35:00 +0200

    Implement OpenPositionsTable

---
+++ src/pages/Portfolio/OpenPositionsTable/OpenPosition.tsx
@@ -0,0 +1,59 @@
+import { Flex, Text } from "@radix-ui/themes";
+import { StyledCell, StyledRow } from "../../../components/Table";
+import theme from "../../../theme";
+import { OpenPositionData } from "../../../types/positionTypes";
+
+type OpenPositionProps = {
+  position: OpenPositionData;
+};
+
+const OpenPosition: React.FC<OpenPositionProps> = ({ position }) => {
+  const [positionLeverage, positionSide] = position.positionSide
+    ? position.positionSide.split(" ")
+    : [undefined, undefined];
+  const isLong = positionSide === "Long";
+  const isPnLPositive = Number(position.unrealizedPnL) > 0;
+  const isFundingPositive = Number(position.parsedFunding) > 0;
+
+  return (
+    <StyledRow style={{ fontSize: "12px" }}>
+      <StyledCell>{position.marketName}</StyledCell>
+      <StyledCell>{position.size} OVL</StyledCell>
+      <StyledCell>
+        <Flex gap={"6px"}>
+          {positionLeverage}
+          <Text
+            weight={"medium"}
+            style={{ color: isLong ? theme.color.green1 : theme.color.red1 }}
+          >
+            {positionSide}
+          </Text>
+        </Flex>
+      </StyledCell>
+      <StyledCell>{position.entryPrice}</StyledCell>
+      <StyledCell>{position.currentPrice}</StyledCell>
+      <StyledCell>{position.liquidatePrice}</StyledCell>
+      <StyledCell>{position.parsedCreatedTimestamp}</StyledCell>
+      <StyledCell>
+        <Text
+          style={{
+            color: isPnLPositive ? theme.color.green1 : theme.color.red1,
+          }}
+        >
+          {position.unrealizedPnL} OVL
+        </Text>
+      </StyledCell>
+      <StyledCell>
+        <Text
+          style={{
+            color: isFundingPositive ? theme.color.green1 : theme.color.red1,
+          }}
+        >
+          {position.parsedFunding} OVL
+        </Text>
+      </StyledCell>
+    </StyledRow>
+  );
+};
+
+export default OpenPosition;
---
+++ src/pages/Portfolio/OpenPositionsTable/index.tsx
@@ -0,0 +1,120 @@
+import { Flex, Text } from "@radix-ui/themes";
+import useMultichainContext from "../../../providers/MultichainContextProvider/useMultichainContext";
+import useSDK from "../../../hooks/useSDK";
+import { useEffect, useState } from "react";
+import useAccount from "../../../hooks/useAccount";
+import StyledTable from "../../../components/Table";
+import OpenPosition from "./OpenPosition";
+import { Address } from "viem";
+import { useIsNewTxnHash } from "../../../state/trade/hooks";
+import { OpenPositionData } from "../../../types/positionTypes";
+import Loader from "../../../components/Loader";
+import theme from "../../../theme";
+
+const POSITIONS_COLUMNS = [
+  "Market",
+  "Size",
+  "Position",
+  "Entry Price",
+  "Current Price",
+  "Liq. Price",
+  "Created",
+  "Unrealized PnL",
+  "Funding",
+];
+
+const OpenPositionsTable: React.FC = () => {
+  const { chainId } = useMultichainContext();
+  const sdk = useSDK();
+  const { address: account } = useAccount();
+  const isNewTxnHash = useIsNewTxnHash();
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [positions, setPositions] = useState<OpenPositionData[] | undefined>(
+    undefined
+  );
+  const [positionsTotalNumber, setPositionsTotalNumber] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchOpenPositions = async () => {
+      setPositions(undefined);
+      setPositionsTotalNumber(0);
+
+      if (account) {
+        setLoading(true);
+        try {
+          const positions = await sdk.openPositions.transformOpenPositions(
+            currentPage,
+            itemsPerPage,
+            undefined,
+            account as Address
+          );
+
+          positions && setPositions(positions.data);
+          const positionsLength = positions && positions.total;
+          positionsLength && setPositionsTotalNumber(positionsLength);
+        } catch (error) {
+          console.error("Error fetching open positions:", error);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchOpenPositions();
+  }, [
+    chainId,
+    account,
+    isNewTxnHash,
+    currentPage,
+    itemsPerPage,
+    setItemsPerPage,
+    setCurrentPage,
+  ]);
+
+  return (
+    <Flex
+      direction={"column"}
+      px={"8px"}
+      pt={"16px"}
+      pb={"66px"}
+      width={"100%"}
+      style={{
+        borderBottom: `1px solid ${theme.color.darkBlue}`,
+      }}
+    >
+      <Text weight={"bold"} size={"5"}>
+        Open Positions
+      </Text>
+
+      <StyledTable
+        headerColumns={POSITIONS_COLUMNS}
+        minWidth="950px"
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+        positionsTotalNumber={positionsTotalNumber}
+        setCurrentPage={setCurrentPage}
+        setItemsPerPage={setItemsPerPage}
+        body={
+          positions &&
+          positions.map((position: OpenPositionData, index: number) => (
+            <OpenPosition position={position} key={index} />
+          ))
+        }
+      />
+
+      {loading ? (
+        <Loader />
+      ) : account ? (
+        positions &&
+        positionsTotalNumber === 0 && <Text>You have no open positions</Text>
+      ) : (
+        <Text style={{ color: theme.color.grey3 }}>No wallet connected</Text>
+      )}
+    </Flex>
+  );
+};
+
+export default OpenPositionsTable;
