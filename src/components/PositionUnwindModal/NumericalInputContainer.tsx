1af7d66e1686e576b13008217bc9d27988b5b129
Aliona-D <alona.danyliuk@inventorsoft.co>
2024-11-06 13:46:57 +0200

    Add NumericalInputContainer to PositionUnwindModal

---
+++ src/components/PositionUnwindModal/NumericalInputContainer.tsx
@@ -0,0 +1,39 @@
+import { Flex, Text } from "@radix-ui/themes";
+import NumericalInput from "../NumericalInput";
+import theme from "../../theme";
+
+type NumericalInputContainerProps = {
+  inputValue: string;
+  handleUserInput: (value: string) => void;
+};
+
+const NumericalInputContainer: React.FC<NumericalInputContainerProps> = ({
+  inputValue,
+  handleUserInput,
+}) => {
+  return (
+    <Flex direction={"column"} gap={"4px"}>
+      <Flex
+        p={"8px"}
+        gap={"10px"}
+        style={{
+          border: `1px solid ${theme.color.grey1}`,
+          borderRadius: "4px",
+        }}
+      >
+        <Text> OVL </Text>
+        <NumericalInput
+          value={inputValue}
+          handleUserInput={(input) => handleUserInput(input)}
+          align={"right"}
+          isFocused={true}
+        />
+      </Flex>
+      <Flex justify={"end"}>
+        <Text style={{ fontSize: "12px" }}>minimum: 0.01%</Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default NumericalInputContainer;
