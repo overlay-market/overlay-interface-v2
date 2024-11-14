e2f5255128ef8aabef9528e4cf200e992abaaf9e
Aliona-D <alona.danyliuk@inventorsoft.co>
2024-11-14 15:58:24 +0200

    Add unwind option for open positions on Trade page

--- src/pages/Trade/PositionsTable/OpenPosition.tsx
+++ src/pages/Trade/PositionsTable/OpenPosition.tsx
@@ -2,44 +2,65 @@ import { Flex, Text } from "@radix-ui/themes";
 import { StyledCell, StyledRow } from "../../../components/Table";
 import theme from "../../../theme";
 import { OpenPositionData } from "overlay-sdk";
+import { useState } from "react";
+import PositionUnwindModal from "../../../components/PositionUnwindModal";
 
 type OpenPositionProps = {
   position: OpenPositionData;
 };
 
 const OpenPosition: React.FC<OpenPositionProps> = ({ position }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPosition, setSelectedPosition] =
+    useState<OpenPositionData | null>(null);
+
   const [positionLeverage, positionSide] = position.positionSide
     ? position.positionSide.split(" ")
     : [undefined, undefined];
   const isLong = positionSide === "Long";
   const isPnLPositive = Number(position.unrealizedPnL) > 0;
 
+  const handleItemClick = () => {
+    setSelectedPosition(position);
+    setShowModal(true);
+  };
+
   return (
-    <StyledRow>
-      <StyledCell>{position.size} OVL</StyledCell>
-      <StyledCell>
-        <Flex gap={"6px"}>
-          {positionLeverage}
+    <>
+      <StyledRow onClick={handleItemClick}>
+        <StyledCell>{position.size} OVL</StyledCell>
+        <StyledCell>
+          <Flex gap={"6px"}>
+            {positionLeverage}
+            <Text
+              weight={"medium"}
+              style={{ color: isLong ? theme.color.green1 : theme.color.red1 }}
+            >
+              {positionSide}
+            </Text>
+          </Flex>
+        </StyledCell>
+        <StyledCell>{position.entryPrice}</StyledCell>
+        <StyledCell>{position.liquidatePrice}</StyledCell>
+        <StyledCell>
           <Text
-            weight={"medium"}
-            style={{ color: isLong ? theme.color.green1 : theme.color.red1 }}
+            style={{
+              color: isPnLPositive ? theme.color.green1 : theme.color.red1,
+            }}
           >
-            {positionSide}
+            {position.unrealizedPnL} OVL
           </Text>
-        </Flex>
-      </StyledCell>
-      <StyledCell>{position.entryPrice}</StyledCell>
-      <StyledCell>{position.liquidatePrice}</StyledCell>
-      <StyledCell>
-        <Text
-          style={{
-            color: isPnLPositive ? theme.color.green1 : theme.color.red1,
-          }}
-        >
-          {position.unrealizedPnL} OVL
-        </Text>
-      </StyledCell>
-    </StyledRow>
+        </StyledCell>
+      </StyledRow>
+
+      {selectedPosition && (
+        <PositionUnwindModal
+          open={showModal}
+          position={selectedPosition}
+          handleDismiss={() => setShowModal(false)}
+        />
+      )}
+    </>
   );
 };
 
