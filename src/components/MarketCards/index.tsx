b3f1ac853e827d2fdd0dab081287a2be7caf8aa5
AndresImach <andyci435@gmail.com>
2024-11-20 12:56:44 -0300

    Add redirect functionality to MarketCards

--- src/components/MarketCards/index.tsx
+++ src/components/MarketCards/index.tsx
@@ -6,6 +6,7 @@ import {
 } from "./market-cards-styles";
 import { Skeleton } from "@radix-ui/themes";
 import { MARKETS_FULL_LOGOS } from "../../constants/markets";
+import useRedirectToTradePage from "../../hooks/useRedirectToTradePage";
 
 interface MarketCardsProps {
   value: string | number | undefined;
@@ -15,6 +16,7 @@ interface MarketCardsProps {
 }
 
 const MarketCards = ({ value, title, id, currency }: MarketCardsProps) => {
+  const redirectToTradePage = useRedirectToTradePage();
   return (
     <Skeleton loading={false}>
       <CustomCard
@@ -22,7 +24,9 @@ const MarketCards = ({ value, title, id, currency }: MarketCardsProps) => {
           backgroundImage: `url(${MARKETS_FULL_LOGOS[id]})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
+          cursor: "pointer",
         }}
+        onClick={() => redirectToTradePage(id)}
       >
         <CardContent direction="column" align="center">
           <CardsValue>{currency + value}</CardsValue>
