980b2de7751e65d4cb0d65a18ab2d5d705b49fda
AndresImach <andyci435@gmail.com>
2024-11-20 16:03:41 -0300

    Add redirect redirect to PromotedBanner,  and update MarketsHeader

--- src/pages/Markets/MarketsHeader.tsx
+++ src/pages/Markets/MarketsHeader.tsx
@@ -1,14 +1,14 @@
 import { Flex, Text, Box } from "@radix-ui/themes";
-import theme from "../../theme";
 import { MarketInfoContainer } from "../Trade/TradeHeader/trade-header-styles";
 
 const MarketsHeader = ({ ovSupply }: { ovSupply: number | undefined }) => {
   return (
     <MarketInfoContainer>
       <Flex direction="row" align={"center"} width={"100%"} height={"100%"}>
-        <Box width={"150px"}>
+        <Flex width={"150px"} justify={"center"} direction="column" ml={"5"}>
           <Text>OV PRICE</Text>
-        </Box>
+          <Text>$~~</Text>
+        </Flex>
 
         <Flex
           width={"114px"}
@@ -16,16 +16,12 @@ const MarketsHeader = ({ ovSupply }: { ovSupply: number | undefined }) => {
           justify={"center"}
           direction="column"
           p={"12px"}
-          flexShrink={"0"}
-          style={{
-            borderRight: `1px solid ${theme.color.darkBlue}`,
-          }}
         >
           <Text>OV SUPPLY</Text>
           <Text>{ovSupply?.toString()}</Text>
         </Flex>
 
-        <Box width={"150px"}>
+        <Box width={"150px"} display={"none"}>
           <Text>GOV</Text>
         </Box>
       </Flex>
