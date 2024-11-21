110e763ad5a596ac013cc134921005589d6ecafb
AndresImach <andyci435@gmail.com>
2024-11-21 14:36:14 -0300

    refactor useMarkets7d hook to simplify data fetching and update market data structure

--- src/hooks/useMarkets7d.ts
+++ src/hooks/useMarkets7d.ts
@@ -1,17 +1,15 @@
-import { useState, useEffect, useCallback, useRef } from "react";
-import useSDK from "./useSDK";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { MARKET_CHART_URL } from "../constants/applications";
 
 interface MarketDataPoint {
-  _id: {
-    market: string;
-    time: string;
-  };
-  high: number;
-  low: number;
-  open: number;
-  close: number;
+  _id: string;
+  latestPrice: number;
+  priceSevenDaysAgo: number;
+  marketId: string;
+  priceOneHourAgo: number;
+  priceOneDayAgo: number;
+  sevenDaysChartData: number[];
 }
 
 interface MarketDataWithOpenPrice {
@@ -19,107 +17,81 @@ interface MarketDataWithOpenPrice {
   latestPrice?: number;
   oneHourChange?: number;
   twentyFourHourChange?: number;
-  sevenDayChange?: number;
-  sevenDaysChartData?: number[];
+  sevenDayChange?: number[];
 }
 
 export function useMarkets7d(marketIds: string[]): MarketDataWithOpenPrice[] {
   const [marketsData, setMarketsData] = useState<MarketDataWithOpenPrice[]>([]);
-  const sdk = useSDK();
   const marketIdsRef = useRef(marketIds);
 
-  const fetchMarketData = useCallback(async () => {
-    const sevenDaysAgo = new Date(
-      Date.now() - 7 * 24 * 60 * 60 * 1000
-    ).toISOString();
+  useEffect(() => {
+    const fetchMarketData = async () => {
+      try {
+        const updatedMarketsData = await Promise.all(
+          marketIdsRef.current.map(async (marketId) => {
+            try {
+              const responseOverview = await axios.get<MarketDataPoint[]>(
+                `${MARKET_CHART_URL.SEPOLIA}/marketsPricesOverview`
+              );
 
-    try {
-      const updatedMarketsData = await Promise.all(
-        marketIdsRef.current.map(async (marketId) => {
-          try {
-            const response2 = await axios.get(
-              `https://api.overlay.market/data/api/markets/${marketId}`
-            );
+              const chartData = responseOverview.data;
 
-            const marketAddressSepolia = response2.data.chains
-              .find(
-                (chain: { chainId: number; deploymentAddress?: string }) =>
-                  chain.chainId === 421614
-              )
-              ?.deploymentAddress.toLowerCase();
+              console.log("chartData", chartData);
 
-            const binSize = 15;
-
-            const response = await axios.get<MarketDataPoint[]>(
-              MARKET_CHART_URL.SEPOLIA,
-              {
-                params: {
-                  market: marketAddressSepolia,
-                  binSize: binSize,
-                  binUnit: "minute",
-                  from: sevenDaysAgo,
-                },
+              if (chartData.length === 0) {
+                throw new Error(`No chart data available for ${marketId}`);
               }
-            );
 
-            const chartData = response.data;
+              const latestDataPoint =
+                chartData[chartData.length - 1]?.latestPrice;
+              const oneHourDataPoint =
+                chartData[chartData.length - 1]?.priceOneHourAgo;
+              const twentyFourHoursDataPoint =
+                chartData[chartData.length - 1]?.priceOneDayAgo;
+              const sevenDaysChartData = chartData.map(
+                (dataPoint) => dataPoint.latestPrice
+              );
 
-            if (chartData.length === 0) {
-              throw new Error(`No chart data available for ${marketId}`);
+              return {
+                marketId,
+                latestPrice: latestDataPoint,
+                oneHourChange: oneHourDataPoint,
+                twentyFourHourChange: twentyFourHoursDataPoint,
+                sevenDayChange: sevenDaysChartData,
+              };
+            } catch (err) {
+              console.error(`Error fetching market data for ${marketId}:`, err);
+              return {
+                marketId,
+              };
             }
+          })
+        );
 
-            // Get the latest data point
-            const latestDataPoint = chartData[chartData.length - 1];
-            const oneHourDataPoint = chartData[chartData.length - 60 / binSize];
-            const twentyFourHoursDataPoint =
-              chartData[chartData.length - (24 * 60) / binSize];
-            const sevenDaysChartData = chartData
-              .filter(
-                (_, index) => index % Math.floor(chartData.length / 20) === 0
-              )
-              .map((dataPoint) => dataPoint.close / chartData[0].close);
+        updatedMarketsData.length > 0 && setMarketsData(updatedMarketsData);
+      } catch (error) {
+        console.error("Error fetching market data:", error);
+      }
+    };
 
-            return {
-              marketId,
-              latestPrice: latestDataPoint.close,
-              oneHourChange:
-                (100 * (latestDataPoint.close - oneHourDataPoint.close)) /
-                oneHourDataPoint.close,
-              twentyFourHourChange:
-                (100 *
-                  (latestDataPoint.close - twentyFourHoursDataPoint.close)) /
-                twentyFourHoursDataPoint.close,
-              sevenDayChange:
-                (100 * (latestDataPoint.close - chartData[0].close)) /
-                chartData[0].close,
-              sevenDaysChartData,
-            };
-          } catch (err) {
-            console.error(`Error fetching market data for ${marketId}:`, err);
-            return {
-              marketId,
-            };
-          }
-        })
-      );
-
-      updatedMarketsData.length > 0 && setMarketsData(updatedMarketsData);
-    } catch (error) {
-      console.error("Error fetching market data:", error);
-    }
-  }, [sdk.markets]);
-
-  useEffect(() => {
-    marketIdsRef.current = marketIds;
-  }, [marketIds]);
-
-  useEffect(() => {
     fetchMarketData();
 
     const intervalId = setInterval(fetchMarketData, 300000); // 5 minutes
 
     return () => clearInterval(intervalId);
-  }, [fetchMarketData]);
+  }, [marketIdsRef]);
+
+  useEffect(() => {
+    marketIdsRef.current = marketIds;
+  }, [marketIds]);
+
+  // useEffect(() => {
+  //   fetchMarketData();
+
+  //   const intervalId = setInterval(fetchMarketData, 300000); // 5 minutes
+
+  //   return () => clearInterval(intervalId);
+  // }, [fetchMarketData]);
 
   return marketsData;
 }
