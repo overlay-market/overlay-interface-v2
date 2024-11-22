7f85d97a370186d227cc62785cebf00dc3dd2157
AndresImach <andyci435@gmail.com>
2024-11-22 13:01:30 -0300

    style(AppContainer): adjust padding for improved layout and responsiveness

--- src/app-styles.ts
+++ src/app-styles.ts
@@ -3,12 +3,15 @@ import styled from "styled-components";
 import theme from "./theme";
 
 export const AppContainer = styled(Flex)`
-  width: 100vw;
-  padding-right: 16px;
+  width: calc(100vw);
+  padding-right: 12px;
+  padding-left: 12px;
   min-height: 100vh;
   background: ${theme.color.background};
 
   @media (min-width: ${theme.breakpoints.sm}) {
     width: calc(100vw - 20px);
+    padding-right: 20px;
+    padding-left: 0px;
   }
 `;
