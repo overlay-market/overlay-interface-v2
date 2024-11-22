b89aff9bef7cdca39bb93be398e936389c1899b7
AndresImach <andyci435@gmail.com>
2024-11-22 09:25:44 -0300

    refactor(AppContainer): adjust width and padding for improved layout

--- src/app-styles.ts
+++ src/app-styles.ts
@@ -3,13 +3,14 @@ import styled from "styled-components";
 import theme from "./theme";
 
 export const AppContainer = styled(Flex)`
-  width: calc(100vw - 16px);
+  width: 100vw;
   padding-right: 16px;
+  padding-left: 16px;
   min-height: 100vh;
   background: ${theme.color.background};
-  
+
   @media (min-width: ${theme.breakpoints.sm}) {
     width: calc(100vw - 20px);
     padding-right: 20px;
   }
-`
+`;
