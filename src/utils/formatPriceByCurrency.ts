b846a9d5d53e9794725f2bb46cccbee8d66f2d55
TomasCImach <tomas90@gmail.com>
2024-11-22 10:38:14 -0300

    add optional significantFigures

--- src/utils/formatPriceByCurrency.ts
+++ src/utils/formatPriceByCurrency.ts
@@ -1,7 +1,7 @@
 import { limitDigitsInDecimals, toPercentUnit, toScientificNumber } from "overlay-sdk";
 
-export const formatPriceByCurrency = (price: string | number, priceCurrency: string): string => {
+export const formatPriceByCurrency = (price: string | number, priceCurrency: string, significantFigures?: number): string => {
   return priceCurrency === '%' 
       ? toPercentUnit(price).toString() 
-      : toScientificNumber(limitDigitsInDecimals(price.toString().replaceAll(",", "")));
+      : toScientificNumber(limitDigitsInDecimals(price.toString().replaceAll(",", "")), significantFigures);
 }
