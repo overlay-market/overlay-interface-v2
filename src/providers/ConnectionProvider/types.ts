d5126287d849255439c11214760d54de3cad6884
TomasCImach <tomas90@gmail.com>
2024-11-25 14:37:20 -0300

    set up connectkit config, and remove deprecated

--- src/providers/ConnectionProvider/types.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import {
-  ResolvedRegister,
-  UseConnectReturnType,
-} from "wagmi";
-
-export const ConnectionContext = createContext<
-  UseConnectReturnType<ResolvedRegister["config"]> | undefined
->(undefined);
