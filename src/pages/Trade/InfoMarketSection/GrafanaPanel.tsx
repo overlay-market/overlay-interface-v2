5d1822afc9d8741f9fb7919a2b99fe8b82d06927
Aliona-D <alona.danyliuk@inventorsoft.co>
2024-11-29 19:00:03 +0200

    Add responsive height adjustment for iframe in GrafanaPanel component

--- src/pages/Trade/InfoMarketSection/GrafanaPanel.tsx
+++ src/pages/Trade/InfoMarketSection/GrafanaPanel.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Flex, Skeleton, Text } from "@radix-ui/themes";
 import { useCurrentMarketState } from "../../../state/currentMarket/hooks";
+import { useMediaQuery } from "../../../hooks/useMediaQuery";
 
 const GrafanaPanel: React.FC = () => {
   const { currentMarket } = useCurrentMarketState();
   const snapshotUrl = `https://grafana.overlay.market/stats-snapshot/${
     currentMarket?.id.toLowerCase() ?? ""
   }?kiosk`; // whole dashboard
+
+  const isDesktop = useMediaQuery("(min-width: 1280px)");
 
   return (
     <Flex width={"100%"} direction={{ initial: "column" }} gap="0px">
@@ -17,12 +20,14 @@ const GrafanaPanel: React.FC = () => {
         <iframe
           src={snapshotUrl}
           width="100%"
-          height="920"
-          frameBorder="0"
+          height={isDesktop ? "860" : "920"}
           title="Grafana Snapshot"
+          style={{
+            border: "none",
+          }}
         ></iframe>
       ) : (
-        <Skeleton height="920px" width="100%" />
+        <Skeleton height={isDesktop ? "860px" : "920px"} width="100%" />
       )}
     </Flex>
   );
