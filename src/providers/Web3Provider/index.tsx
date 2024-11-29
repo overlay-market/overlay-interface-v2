2b608bad184515ba3075fb2e0247fe6077d82b57
TomasCImach <tomas90@gmail.com>
2024-11-29 10:13:07 -0300

    add TOS to connect wallet modal

--- src/providers/Web3Provider/index.tsx
+++ src/providers/Web3Provider/index.tsx
@@ -10,7 +10,23 @@ const Web3Provider: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider>
+        <ConnectKitProvider 
+          options={{
+            disclaimer: (
+              <>
+                Perpetuals are not available to anyone residents of, or are located, incorporated, or having a registered agent in, the United States or a restricted territory as defined in Overlay's {" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://overlay.market/#/tos"
+                >
+                  Terms of Service
+                </a>{" "}
+                (the "TOS"). By connecting your wallet, you agree to the TOS.
+              </>
+            ),
+          }}
+        >
           {children}
         </ConnectKitProvider>
       </QueryClientProvider>
