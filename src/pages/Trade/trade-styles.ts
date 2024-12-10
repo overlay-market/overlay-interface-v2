7b658a4b462ada11a407d37fa011484c79113034
AndresImach <andyci435@gmail.com>
2024-12-10 11:38:03 -0300

    remove unused LineSeparator styled component from Trade styles

--- src/pages/Trade/trade-styles.ts
+++ src/pages/Trade/trade-styles.ts
@@ -13,19 +13,3 @@ export const TradeContainer = styled(Flex)`
     padding-left: 16px;
   }
 `;
-
-export const LineSeparator = styled(Flex)`
-  @media (min-width: ${theme.breakpoints.sm}) {
-    height: 0;
-    width: calc(100% - ${theme.headerSize.tabletWidth});
-    position: absolute;
-    top: ${theme.headerSize.height};
-    left: ${theme.headerSize.tabletWidth};
-    border-bottom: 1px solid ${theme.color.darkBlue};
-  }
-
-  @media (min-width: ${theme.breakpoints.md}) {
-    width: calc(100% - ${theme.headerSize.width});
-    left: ${theme.headerSize.width};
-  }
-`;
