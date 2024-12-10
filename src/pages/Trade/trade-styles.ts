68d344dd18c3e9faa3ff3e37bc5fbc2bef934a0d
AndresImach <andyci435@gmail.com>
2024-12-10 10:49:37 -0300

    refactor: adjust padding in Portfolio and Trade components for improved layout

--- src/pages/Trade/trade-styles.ts
+++ src/pages/Trade/trade-styles.ts
@@ -10,7 +10,22 @@ export const StyledFlex = styled(Flex)`
 
 export const TradeContainer = styled(Flex)`
   @media (min-width: ${theme.breakpoints.xxl}) {
-    padding-left: 60px;
-    padding-right: 28px;
+    padding-left: 16px;
   }
-`
+`;
+
+export const LineSeparator = styled(Flex)`
+  @media (min-width: ${theme.breakpoints.sm}) {
+    height: 0;
+    width: calc(100% - ${theme.headerSize.tabletWidth});
+    position: absolute;
+    top: ${theme.headerSize.height};
+    left: ${theme.headerSize.tabletWidth};
+    border-bottom: 1px solid ${theme.color.darkBlue};
+  }
+
+  @media (min-width: ${theme.breakpoints.md}) {
+    width: calc(100% - ${theme.headerSize.width});
+    left: ${theme.headerSize.width};
+  }
+`;
