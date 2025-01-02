a785e6cd62ae57b093ed066181989363bd128212
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-01-02 13:18:11 +0200

    Implement TradeMarket Card

---
+++ src/components/MarketCards/TradeMarketCard.tsx
@@ -0,0 +1,88 @@
+import { Flex, Text } from "@radix-ui/themes";
+import { MARKETS_FULL_LOGOS } from "../../constants/markets";
+import useRedirectToTradePage from "../../hooks/useRedirectToTradePage";
+import {
+  CardImg,
+  CustomCard,
+  MarketDescription,
+  MarketTitle,
+} from "./trade-market-card-styles";
+import theme from "../../theme";
+
+interface TradeMarketCardProps {
+  priceWithCurrency: string;
+  title: string;
+  id: string;
+  description?: string;
+  h24: string;
+  funding?: string;
+}
+
+const TradeMarketCard = ({
+  priceWithCurrency,
+  title,
+  id,
+  description,
+  h24,
+  funding,
+}: TradeMarketCardProps) => {
+  const redirectToTradePage = useRedirectToTradePage();
+
+  return (
+    <CustomCard onClick={() => redirectToTradePage(id)}>
+      <CardImg style={{ backgroundImage: `url(${MARKETS_FULL_LOGOS[id]})` }} />
+
+      <Flex
+        direction={"column"}
+        px={"8px"}
+        py={"12px"}
+        gap={"8px"}
+        justify={"between"}
+        height={"100%"}
+      >
+        <Flex direction={"column"} gap={"8px"}>
+          <Text
+            weight={"bold"}
+            size={"1"}
+            style={{ color: theme.color.green2 }}
+          >
+            {priceWithCurrency}
+          </Text>
+          <MarketTitle weight={"medium"} size={"3"}>
+            {title}
+          </MarketTitle>
+          <MarketDescription size={"1"}>{description}</MarketDescription>
+        </Flex>
+
+        <Flex direction={"column"} gap={"4px"} mt={"auto"}>
+          <Flex justify={"between"}>
+            <Text size={"1"} style={{ color: theme.color.white2 }}>
+              24h:
+            </Text>
+            <Text
+              weight={"bold"}
+              size={"1"}
+              style={{ color: theme.color.red1 }}
+            >
+              {h24}
+            </Text>
+          </Flex>
+          <Flex justify={"between"}>
+            <Text size={"1"} style={{ color: theme.color.white2 }}>
+              Funding:
+            </Text>
+            <Text
+              weight={"bold"}
+              size={"1"}
+              style={{ color: theme.color.green3 }}
+            >
+              {funding}
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+    </CustomCard>
+  );
+};
+
+export default TradeMarketCard;
---
+++ src/components/MarketCards/trade-market-card-styles.ts
@@ -0,0 +1,45 @@
+import { Flex, Text } from "@radix-ui/themes";
+import styled from "styled-components";
+import theme from "../../theme";
+
+export const CustomCard = styled(Flex)`
+  display: flex;
+  flex-direction: column;
+  width: 172px;
+  height: 273px;
+  border-radius: 8px 8px 16px 16px;
+  border: 1px solid ${theme.color.darkBlue};
+  background-color: ${theme.color.grey7};
+  cursor: pointer;
+  overflow: hidden;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  &:hover {
+    transform: scale(1.02);
+  }
+`;
+
+export const CardImg = styled(Flex)`
+  width: 100%;
+  height: 97px;
+  min-height: 97px;
+  background-size: cover;
+  background-position: center;
+  
+`;
+
+export const MarketTitle = styled(Text)`
+  color: ${theme.color.white};
+  line-height: 16px;
+  letter-spacing: -0.04em;  
+`;
+
+export const MarketDescription = styled(Text)`
+  color: ${theme.color.white1};
+  line-height: 16.8px;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
