17c6561a48a477557c4d3ac093f81baa1e3587df
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-01-14 14:02:14 +0200

    Add horizontal scrollbar to eligibility table

--- src/pages/Airdrops/EligibilitySection/index.tsx
+++ src/pages/Airdrops/EligibilitySection/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@radix-ui/themes";
+import { Flex, ScrollArea, Text } from "@radix-ui/themes";
 import React, { useEffect, useState } from "react";
 import theme from "../../../theme";
 import {
@@ -60,6 +60,8 @@ const EligibilitySection: React.FC<Props> = ({
     }
   };
 
+  const minWidthTable = (Object.keys(airdrops).length + 1) * 150;
+
   return (
     <Flex direction={"column"} pb="100px">
       <Flex direction="column" gap="8px" pb="32px">
@@ -71,160 +73,167 @@ const EligibilitySection: React.FC<Props> = ({
         </Text>
       </Flex>
 
-      <Table>
-        <thead>
-          <tr>
-            <StyledHeader textalign={"left"}>Address</StyledHeader>
-            {airdrops &&
-              Object.keys(airdrops).map((airdropId: string) => {
-                return (
-                  <StyledHeader key={airdropId} textalign={"right"}>
-                    <AirdropIdWithTooltip airdrop={airdrops[airdropId]} />
-                  </StyledHeader>
-                );
-              })}
-          </tr>
-        </thead>
+      <ScrollArea
+        type="auto"
+        scrollbars="horizontal"
+        size="2"
+        style={{ paddingBottom: "16px" }}
+      >
+        <Table style={{ minWidth: `${minWidthTable}px` }}>
+          <thead>
+            <tr>
+              <StyledHeader textalign={"left"}>Address</StyledHeader>
+              {airdrops &&
+                Object.keys(airdrops).map((airdropId: string) => {
+                  return (
+                    <StyledHeader key={airdropId} textalign={"right"}>
+                      <AirdropIdWithTooltip airdrop={airdrops[airdropId]} />
+                    </StyledHeader>
+                  );
+                })}
+            </tr>
+          </thead>
 
-        <tbody>
-          {addressAirdropRows &&
-            Object.keys(addressAirdropRows).map((address) => (
-              <StyledRow key={address}>
-                <StyledCell textalign="left">
-                  <Flex
-                    gap="10px"
-                    align={"center"}
-                    style={{ cursor: "pointer" }}
-                    onClick={() => copyToClipboardHandle(address)}
+          <tbody>
+            {addressAirdropRows &&
+              Object.keys(addressAirdropRows).map((address) => (
+                <StyledRow key={address}>
+                  <StyledCell textalign="left">
+                    <Flex
+                      gap="10px"
+                      align={"center"}
+                      style={{ cursor: "pointer" }}
+                      onClick={() => copyToClipboardHandle(address)}
+                    >
+                      <Text>{displayAddress(address)}</Text>
+                      <CopyIconWrapper>
+                        <CopyIcon />
+
+                        {copiedStatus && copiedAddress === address && (
+                          <CopiedBox>{copiedStatus}</CopiedBox>
+                        )}
+                      </CopyIconWrapper>
+                    </Flex>
+                  </StyledCell>
+                  {addressAirdropRows[address].map((value: string, ind) => {
+                    return (
+                      <StyledCell key={ind} textalign="right">
+                        <Text>{value}</Text>
+                      </StyledCell>
+                    );
+                  })}
+                </StyledRow>
+              ))}
+
+            {totalAmountValues &&
+              addressAirdropRows &&
+              Object.keys(addressAirdropRows).length !== 0 && (
+                <StyledRow>
+                  <StyledCell
+                    id="totalamount"
+                    textalign="left"
+                    style={{
+                      width:
+                        Object.keys(airdrops).length === 1 ? "160px" : "135px",
+                    }}
                   >
-                    <Text>{displayAddress(address)}</Text>
-                    <CopyIconWrapper>
-                      <CopyIcon />
-
-                      {copiedStatus && copiedAddress === address && (
-                        <CopiedBox>{copiedStatus}</CopiedBox>
-                      )}
-                    </CopyIconWrapper>
-                  </Flex>
-                </StyledCell>
-                {addressAirdropRows[address].map((value: string, ind) => {
-                  return (
-                    <StyledCell key={ind} textalign="right">
-                      <Text>{value}</Text>
-                    </StyledCell>
-                  );
-                })}
-              </StyledRow>
-            ))}
-
-          {totalAmountValues &&
-            addressAirdropRows &&
-            Object.keys(addressAirdropRows).length !== 0 && (
-              <StyledRow>
-                <StyledCell
-                  id="totalamount"
-                  textalign="left"
-                  style={{
-                    width:
-                      Object.keys(airdrops).length === 1 ? "160px" : "135px",
-                  }}
-                >
-                  <TotalAmountCell>
-                    <Text style={{ color: theme.color.grey3 }}>
-                      Total amount
-                    </Text>
-                    {Object.keys(airdrops).length === 1 &&
-                      (AIRDROPS[Object.keys(airdrops)[0]].status ===
-                      AirdropStatus.COMING_SOON ? (
-                        <Text style={{ color: theme.color.blue2 }}>
-                          Coming soon
-                        </Text>
-                      ) : (
-                        <StyledLink
-                          to={`/claim/${
-                            MERKLE_DISTIBUTOR_ADDRESSES[
-                              Object.keys(airdrops)[0]
-                            ]
-                          }`}
-                          onClick={(event) =>
-                            AIRDROPS[Object.keys(airdrops)[0]].status ===
-                              AirdropStatus.TBA && event.preventDefault()
-                          }
-                          target="_blank"
-                          style={{
-                            color:
+                    <TotalAmountCell>
+                      <Text style={{ color: theme.color.grey3 }}>
+                        Total amount
+                      </Text>
+                      {Object.keys(airdrops).length === 1 &&
+                        (AIRDROPS[Object.keys(airdrops)[0]].status ===
+                        AirdropStatus.COMING_SOON ? (
+                          <Text style={{ color: theme.color.blue2 }}>
+                            Coming soon
+                          </Text>
+                        ) : (
+                          <StyledLink
+                            to={`/claim/${
+                              MERKLE_DISTIBUTOR_ADDRESSES[
+                                Object.keys(airdrops)[0]
+                              ]
+                            }`}
+                            onClick={(event) =>
                               AIRDROPS[Object.keys(airdrops)[0]].status ===
-                              AirdropStatus.TBA
-                                ? theme.color.grey8
-                                : theme.color.blue2,
-                            cursor:
-                              AIRDROPS[Object.keys(airdrops)[0]].status ===
-                              AirdropStatus.TBA
-                                ? "default"
-                                : "pointer",
-                          }}
-                        >
-                          Claim
-                          <OpenInNewIcon />
-                        </StyledLink>
-                      ))}
-                  </TotalAmountCell>
-                </StyledCell>
-                {totalAmountValues.map((value: string | number, ind) => {
-                  return (
-                    <StyledCell key={ind} textalign="right">
-                      <Text>{value}</Text>
-                    </StyledCell>
-                  );
-                })}
-              </StyledRow>
-            )}
+                                AirdropStatus.TBA && event.preventDefault()
+                            }
+                            target="_blank"
+                            style={{
+                              color:
+                                AIRDROPS[Object.keys(airdrops)[0]].status ===
+                                AirdropStatus.TBA
+                                  ? theme.color.grey8
+                                  : theme.color.blue2,
+                              cursor:
+                                AIRDROPS[Object.keys(airdrops)[0]].status ===
+                                AirdropStatus.TBA
+                                  ? "default"
+                                  : "pointer",
+                            }}
+                          >
+                            Claim
+                            <OpenInNewIcon />
+                          </StyledLink>
+                        ))}
+                    </TotalAmountCell>
+                  </StyledCell>
+                  {totalAmountValues.map((value: string | number, ind) => {
+                    return (
+                      <StyledCell key={ind} textalign="right">
+                        <Text>{value}</Text>
+                      </StyledCell>
+                    );
+                  })}
+                </StyledRow>
+              )}
 
-          {addressAirdropRows &&
-            Object.keys(airdrops).length > 1 &&
-            Object.keys(addressAirdropRows).length !== 0 && (
-              <StyledRow>
-                <StyledCell id="claim" textalign="left">
-                  <Text style={{ color: theme.color.grey3 }}>Claim OV</Text>
-                </StyledCell>
-                {Object.keys(AIRDROPS).map((airdropId) => {
-                  return (
-                    <StyledCell key={airdropId} textalign="right">
-                      {AIRDROPS[airdropId].status ===
-                      AirdropStatus.COMING_SOON ? (
-                        <Text style={{ color: theme.color.grey3 }}>
-                          Coming soon
-                        </Text>
-                      ) : (
-                        <StyledLink
-                          to={`/claim/${MERKLE_DISTIBUTOR_ADDRESSES[airdropId]}`}
-                          onClick={(event) =>
-                            AIRDROPS[airdropId].status === AirdropStatus.TBA &&
-                            event.preventDefault()
-                          }
-                          target="_blank"
-                          style={{
-                            color:
-                              AIRDROPS[airdropId].status === AirdropStatus.TBA
-                                ? theme.color.grey8
-                                : theme.color.blue2,
-                            cursor:
-                              AIRDROPS[airdropId].status === AirdropStatus.TBA
-                                ? "default"
-                                : "pointer",
-                          }}
-                        >
-                          Claim
-                          <OpenInNewIcon />
-                        </StyledLink>
-                      )}
-                    </StyledCell>
-                  );
-                })}
-              </StyledRow>
-            )}
-        </tbody>
-      </Table>
+            {addressAirdropRows &&
+              Object.keys(airdrops).length > 1 &&
+              Object.keys(addressAirdropRows).length !== 0 && (
+                <StyledRow>
+                  <StyledCell id="claim" textalign="left">
+                    <Text style={{ color: theme.color.grey3 }}>Claim OV</Text>
+                  </StyledCell>
+                  {Object.keys(AIRDROPS).map((airdropId) => {
+                    return (
+                      <StyledCell key={airdropId} textalign="right">
+                        {AIRDROPS[airdropId].status ===
+                        AirdropStatus.COMING_SOON ? (
+                          <Text style={{ color: theme.color.grey3 }}>
+                            Coming soon
+                          </Text>
+                        ) : (
+                          <StyledLink
+                            to={`/claim/${MERKLE_DISTIBUTOR_ADDRESSES[airdropId]}`}
+                            onClick={(event) =>
+                              AIRDROPS[airdropId].status ===
+                                AirdropStatus.TBA && event.preventDefault()
+                            }
+                            target="_blank"
+                            style={{
+                              color:
+                                AIRDROPS[airdropId].status === AirdropStatus.TBA
+                                  ? theme.color.grey8
+                                  : theme.color.blue2,
+                              cursor:
+                                AIRDROPS[airdropId].status === AirdropStatus.TBA
+                                  ? "default"
+                                  : "pointer",
+                            }}
+                          >
+                            Claim
+                            <OpenInNewIcon />
+                          </StyledLink>
+                        )}
+                      </StyledCell>
+                    );
+                  })}
+                </StyledRow>
+              )}
+          </tbody>
+        </Table>
+      </ScrollArea>
 
       {(!addressAirdropRows ||
         Object.keys(addressAirdropRows).length === 0) && (
