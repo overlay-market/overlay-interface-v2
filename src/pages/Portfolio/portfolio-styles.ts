81950625cff57cb2d953d71839aee3227adab58d
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-01-28 20:48:59 +0200

    Update styles on Portfolio page

--- src/pages/Portfolio/portfolio-styles.ts
+++ src/pages/Portfolio/portfolio-styles.ts
@@ -3,31 +3,37 @@ import styled from "styled-components";
 import theme from "../../theme";
 
 export const HeaderEmptyPlaceholder = styled(Box)`
-  height: 0;
-  margin: 0 3px;
-  border-bottom: 1px solid ${theme.color.darkBlue};
-
   @media (min-width: ${theme.breakpoints.sm}) {
     width: 100%;
-    margin: 0;
     height: ${theme.headerSize.height};
-    border-bottom: none;
   }
 `;
 
 export const PortfolioContainer = styled(Flex)`
+  @media (min-width: ${theme.breakpoints.sm}) {
+    width: calc(100% - ${theme.headerSize.tabletWidth});
+  }
+
   @media (min-width: ${theme.breakpoints.xxl}) {
-    padding-left: 16px;
+    padding-left: ${theme.app.xxlPadding};
+    width: calc(100% - ${theme.app.xxlPadding});
   }
 `;
 
 export const LineSeparator = styled(Flex)`
+  height: 0;
+  margin-left: -${theme.app.mobilePadding};
+  margin-right: -${theme.app.mobilePadding};
+  width: calc(100% + 2 * ${theme.app.mobilePadding});
+  border-bottom: 1px solid ${theme.color.darkBlue};
+  
   @media (min-width: ${theme.breakpoints.sm}) {
     height: 0;
     width: calc(100% - ${theme.headerSize.tabletWidth});
     position: absolute;
     top: ${theme.headerSize.height};
     left: ${theme.headerSize.tabletWidth};
+    margin: 0;
     border-bottom: 1px solid ${theme.color.darkBlue};
   }
 
