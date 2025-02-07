bb129f4763d2546444a4615c891923e0bc813303
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-02-07 17:29:38 +0200

    Add styles and components on Earn page

--- src/pages/Earn/index.tsx
+++ src/pages/Earn/index.tsx
@@ -1,7 +1,39 @@
+import { Flex, Text } from "@radix-ui/themes";
 import React from "react";
+import theme from "../../theme";
+import { EarnContainer, EarnContent, LineSeparator } from "./earn-styles";
+import Overview from "./Overview";
+import Rewards from "./Rewards";
+import StakeTable from "./StakeTable";
 
 const Earn: React.FC = () => {
-  return <div>Earn page</div>;
+  return (
+    <EarnContainer width={"100%"} height={"100%"} direction={"column"}>
+      <Flex
+        display={{ initial: "none", sm: "flex" }}
+        align={"center"}
+        height={theme.headerSize.height}
+        px={"16px"}
+      >
+        <Text size={"2"} weight={"medium"}>
+          Earn
+        </Text>
+      </Flex>
+      <LineSeparator />
+
+      <EarnContent
+        direction={"column"}
+        gap={{ initial: "24px", sm: "28px", md: "16px" }}
+        pt={"16px"}
+        pl={{ initial: "4px", sm: "16px" }}
+        pr={{ initial: "4px", sm: "0px" }}
+      >
+        <Overview />
+        <Rewards />
+        <StakeTable />
+      </EarnContent>
+    </EarnContainer>
+  );
 };
 
 export default Earn;
