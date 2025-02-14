7e029bf689b9a5d43764a9df82c0ff4b7989bc4f
TomasCImach <tomas90@gmail.com>
2025-02-14 12:39:52 -0300

    add image preloading functionality in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,20 @@ import store from "./state/state.tsx";
 import Web3Provider from "./providers/Web3Provider";
 import { ArcxAnalyticsProvider } from '@0xarc-io/analytics';
 
+const images = import.meta.glob("/src/assets/**/*.{png,jpg,jpeg,webp,gif,mp4}", { eager: true });
+
+function preloadImages() {
+  Object.values(images).forEach((img) => {
+    const link = document.createElement("link");
+    link.rel = "preload";
+    link.href = (img as { default: string }).default;
+    link.as = "image";
+    document.head.appendChild(link);
+  });
+}
+
+preloadImages();
+
 const apiKey = "44242b32c3a5151254dc2bdc85fe66dbcc9f70da4c6d3abfca236acb30d9e6e8";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
