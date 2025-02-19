49ad6c239c5c879e71b0bf0303f1caf910a7770d
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-02-19 12:49:04 +0200

    Integrate dynamic vaults data from VaultsState in StakeTable

--- src/pages/Earn/StakeTable/index.tsx
+++ src/pages/Earn/StakeTable/index.tsx
@@ -10,43 +10,21 @@ import Loader from "../../../components/Loader";
 import theme from "../../../theme";
 import { useNavigate } from "react-router-dom";
 import useAccount from "../../../hooks/useAccount";
-import { VAULTS_TOKEN_LOGOS } from "../../../constants/stake";
+import { ACTIVE_VAULTS, VAULTS_TOKEN_LOGOS } from "../../../constants/stake";
 import { useMediaQuery } from "../../../hooks/useMediaQuery";
+import { useVaultsState } from "../../../state/vaults/hooks";
+import useMultichainContext from "../../../providers/MultichainContextProvider/useMultichainContext";
 
 const StakeTable: React.FC = () => {
   const navigate = useNavigate();
   const { address: account } = useAccount();
+  const { chainId } = useMultichainContext();
+  const { vaults, vaultDetails } = useVaultsState();
+
   const isDesktop = useMediaQuery("(min-width: 1280px)");
   const isMobile = useMediaQuery("(max-width: 767px)");
 
-  const vaults = [
-    {
-      vaultId: "USDC-OVL",
-      apr: "20.3%",
-      tvl: "$120,000,000",
-      lockPeriod: "30 Days",
-      rewards: ["USDC", "OVL"],
-    },
-    {
-      vaultId: "OVL",
-      apr: "24.3%",
-      tvl: "$120,000,000",
-      lockPeriod: "30 Days",
-      rewards: ["OVL"],
-    },
-    {
-      vaultId: "BERA-OVL",
-      apr: "14.3%",
-      tvl: "$120,000,000",
-      lockPeriod: "30 Days",
-      rewards: ["BGT", "OVL"],
-    },
-  ];
-
-  const myRewards: Record<string, string[]> = {
-    "USDC-OVL": ["1.25 USDC", "123 OVL"],
-    OVL: ["1.25 OVL"],
-  };
+  const chainVaultNames = ACTIVE_VAULTS[chainId as number] || [];
 
   const redirectToStakePage = (vaultId: string) => {
     navigate(`/earn/${encodeURIComponent(vaultId)}`);
@@ -66,13 +44,13 @@ const StakeTable: React.FC = () => {
             <StyledHeader textalign={"right"}>My Rewards</StyledHeader>
           )}
           <StyledHeader textalign={"right"}>TVL</StyledHeader>
-          {/* {!isMobile && (
+          {!isMobile && (
             <StyledHeader textalign={"right"}>
               Lock-up {!isDesktop && <br />} Period
             </StyledHeader>
-          )} */}
+          )}
           {!isMobile && (
-            <StyledHeader textalign={"right"}>Rewards</StyledHeader>
+            <StyledHeader textalign={"right"}>Daily Rewards</StyledHeader>
           )}
         </tr>
       </thead>
@@ -92,80 +70,159 @@ const StakeTable: React.FC = () => {
             </td>
           </tr>
         )}
+
         {vaults &&
-          vaults.map((vault, index) => (
-            <StyledRow
-              key={index}
-              onClick={() => redirectToStakePage(vault.vaultId)}
-            >
-              <StyledCell textalign="left">
-                <Flex gap={"8px"} align={"center"}>
-                  <Flex
-                    width={{ initial: "34px", lg: "62px" }}
-                    justify={"center"}
-                  >
-                    {VAULTS_TOKEN_LOGOS[
-                      vault.vaultId as keyof typeof VAULTS_TOKEN_LOGOS
-                    ].map((tokenLogo) => (
-                      <TokenImg src={tokenLogo} alt={"token logo"} />
-                    ))}
-                  </Flex>
-                  <Flex
-                    px={{ initial: "8px", lg: "10px" }}
-                    py={{ initial: "4px", sm: "12px" }}
-                  >
-                    <Text weight={"medium"} style={{ lineHeight: "17px" }}>
-                      {isDesktop || vault.vaultId.split("-").length === 1 ? (
-                        `${vault.vaultId} Vault`
-                      ) : (
-                        <Flex direction={"column"}>
-                          <span>{vault.vaultId.split("-")[0]}</span>
-                          <span>{vault.vaultId.split("-")[1]} Vault</span>
-                        </Flex>
-                      )}
-                    </Text>
-                  </Flex>
-                </Flex>
-              </StyledCell>
+          vaults.map((vault) => {
+            const vaultId = vault.stakingPool.toLowerCase();
+            const matchedVault = chainVaultNames.find(
+              (v) => v.vaultAddress.toLowerCase() === vaultId
+            )!;
+            const vaultName = matchedVault.vaultName;
+            const details = vaultDetails?.find(
+              (detail) => detail.vaultAddress.toLowerCase() === vaultId
+            );
+            const totalSupply = details?.totalSupply.toLocaleString() ?? "";
+            const userRewardsObject = details?.userRewards;
+            const userRewards: string[] = [];
 
-              <StyledCell textalign="right">
-                <Text>{vault.apr}</Text>
-              </StyledCell>
+            if (userRewardsObject) {
+              Object.entries(userRewardsObject).forEach(([key, reward]) => {
+                if (key === "rewardA") {
+                  userRewards.push(
+                    `${reward.toLocaleString()} ${
+                      vault.rewardTokenADetail.symbol
+                    }`
+                  );
+                } else if (
+                  key === "rewardB" &&
+                  vault.isDualFactory &&
+                  vault.rewardTokenBDetail
+                ) {
+                  userRewards.push(
+                    `${reward.toLocaleString()} ${
+                      vault.rewardTokenBDetail.symbol
+                    }`
+                  );
+                }
+              });
+            }
 
-              {account && !isMobile && (
+            const timeUnitSeconds: number = Number(vault.rewardsDuration);
+            const secondsInDay = 86400;
+            const timeUnitDays = timeUnitSeconds / secondsInDay;
+            const lockUpPeriod = `${timeUnitDays} ${
+              timeUnitDays === 1 ? "Day" : "Days"
+            }`;
+            console.log(
+              { timeUnitDays },
+              new Date(timeUnitSeconds * 1000),
+              vault.postGeneratedApr
+            );
+
+            const dailyRewards: string[] = [];
+            dailyRewards.push(
+              `${vault.dailyEmissionRewardA.toLocaleString()} ${
+                vault.rewardTokenADetail.symbol
+              }`
+            );
+
+            if (vault.isDualFactory) {
+              dailyRewards.push(
+                `${vault.dailyEmissionRewardB?.toLocaleString()} ${
+                  vault.rewardTokenBDetail?.symbol
+                }`
+              );
+            }
+
+            return (
+              <StyledRow
+                key={vaultId}
+                onClick={() => redirectToStakePage(vaultName)}
+              >
+                <StyledCell textalign="left">
+                  <Flex gap={"8px"} align={"center"}>
+                    <Flex
+                      width={{ initial: "34px", lg: "62px" }}
+                      justify={"center"}
+                    >
+                      {VAULTS_TOKEN_LOGOS[
+                        vaultName as keyof typeof VAULTS_TOKEN_LOGOS
+                      ].map((tokenLogo, idx) => (
+                        <TokenImg
+                          src={tokenLogo}
+                          alt={"token logo"}
+                          key={idx}
+                        />
+                      ))}
+                    </Flex>
+                    <Flex
+                      px={{ initial: "8px", lg: "10px" }}
+                      py={{ initial: "4px", sm: "12px" }}
+                    >
+                      <Text weight={"medium"} style={{ lineHeight: "17px" }}>
+                        {isDesktop || !vaultName.includes("-") ? (
+                          `${vaultName} Vault`
+                        ) : (
+                          <Flex direction={"column"}>
+                            <span>{vaultName.split("-")[0]}</span>
+                            <span>{vaultName.split("-")[1]} Vault</span>
+                          </Flex>
+                        )}
+                      </Text>
+                    </Flex>
+                  </Flex>
+                </StyledCell>
+
                 <StyledCell textalign="right">
-                  <Flex direction={"column"}>
-                    {myRewards &&
-                      myRewards[vault.vaultId as keyof typeof myRewards]
-                        ?.length > 0 &&
-                      myRewards[vault.vaultId as keyof typeof myRewards].map(
-                        (reward, i) => (
+                  <Text>8.23%</Text>
+                </StyledCell>
+
+                {account && !isMobile && (
+                  <StyledCell textalign="right">
+                    <Flex direction={"column"}>
+                      {userRewards &&
+                        userRewards.map((reward, i) => (
                           <Text key={i} style={{ color: theme.color.green2 }}>
                             {reward}
                           </Text>
-                        )
-                      )}
-                  </Flex>
-                </StyledCell>
-              )}
+                        ))}
+                    </Flex>
+                  </StyledCell>
+                )}
 
-              <StyledCell textalign="right">
-                <Text>{vault.tvl}</Text>
-              </StyledCell>
-
-              {/* {!isMobile && (
                 <StyledCell textalign="right">
-                  <Text>{vault.lockPeriod}</Text>
+                  <Text>{totalSupply} OVL</Text>
                 </StyledCell>
-              )} */}
 
-              {!isMobile && (
-                <StyledCell textalign="right">
-                  <Text>{vault.rewards.join(" / ")}</Text>
-                </StyledCell>
-              )}
-            </StyledRow>
-          ))}
+                {!isMobile && (
+                  <StyledCell textalign="right">
+                    <Text>{lockUpPeriod}</Text>
+                  </StyledCell>
+                )}
+
+                {!isMobile && (
+                  <StyledCell textalign="right">
+                    <Flex width={"100%"} justify={"end"}>
+                      <Flex direction={"column"} width={"140px"} align={"end"}>
+                        {dailyRewards &&
+                          dailyRewards.map((reward, i) => (
+                            <Flex
+                              key={i}
+                              justify={"between"}
+                              gap={"8px"}
+                              width={"100%"}
+                            >
+                              <Text>{reward.split(" ")[0]}</Text>
+                              <Text>{reward.split(" ")[1]}</Text>
+                            </Flex>
+                          ))}
+                      </Flex>
+                    </Flex>
+                  </StyledCell>
+                )}
+              </StyledRow>
+            );
+          })}
       </tbody>
     </StyledTable>
   );
