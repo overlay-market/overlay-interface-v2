38026de7865737d7b0b08db0cea2adf10fa8cddb
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-02-20 16:29:23 +0200

    Implement vaultActionHandlers on Earn page

--- src/pages/Earn/index.tsx
+++ src/pages/Earn/index.tsx
@@ -1,12 +1,35 @@
 import { Flex, Text } from "@radix-ui/themes";
-import React from "react";
+import React, { useEffect } from "react";
 import theme from "../../theme";
 import { EarnContainer, EarnContent, LineSeparator } from "./earn-styles";
 import Overview from "./Overview";
 import Rewards from "./Rewards";
 import StakeTable from "./StakeTable";
+import useMultichainContext from "../../providers/MultichainContextProvider/useMultichainContext";
+import { useVaultsActionHandlers } from "../../state/vaults/hooks";
+import { useVaults } from "./hooks/useVaults";
+import { useVaultDetails } from "./hooks/useVaultDetails";
 
 const Earn: React.FC = () => {
+  const { chainId } = useMultichainContext();
+  const { handleVaultsUpdate, handleVaultDetailsUpdate } =
+    useVaultsActionHandlers();
+
+  const vaults = useVaults();
+  const vaultsDetails = useVaultDetails(vaults);
+
+  useEffect(() => {
+    if (vaults) {
+      handleVaultsUpdate(vaults);
+    }
+  }, [chainId, vaults]);
+
+  useEffect(() => {
+    if (vaultsDetails) {
+      handleVaultDetailsUpdate(vaultsDetails);
+    }
+  }, [chainId, vaults, vaultsDetails]);
+
   return (
     <EarnContainer width={"100%"} height={"100%"} direction={"column"}>
       <Flex
