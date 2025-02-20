12a6b4c37c956cf27801cb4503b879007b247a5d
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-02-20 16:42:44 +0200

    Integrate dynamic vaults data from VaultsState in InfoSection

--- src/pages/Earn/StakePage/InfoSection.tsx
+++ src/pages/Earn/StakePage/InfoSection.tsx
@@ -1,5 +1,5 @@
 import { Flex, Text } from "@radix-ui/themes";
-import React from "react";
+import React, { useMemo } from "react";
 import {
   InfoBox,
   RewardBox,
@@ -8,10 +8,65 @@ import {
 } from "./info-section-styles";
 import theme from "../../../theme";
 import { useParams } from "react-router-dom";
-import { VAULTS_TOKEN_LOGOS } from "../../../constants/stake";
+import useMultichainContext from "../../../providers/MultichainContextProvider/useMultichainContext";
+import {
+  getTokenLogo,
+  getVaultAddressByVaultName,
+} from "../utils/currentVaultdata";
+import {
+  useCurrentVault,
+  useCurrentVaultDetails,
+} from "../hooks/useCurrentVaultData";
+import { formatReward } from "../utils/formatReward";
 
 const InfoSection: React.FC = () => {
   const { vaultId } = useParams();
+  const { chainId } = useMultichainContext();
+
+  const vaultAddress = getVaultAddressByVaultName(chainId, vaultId);
+  const currentVault = useCurrentVault(vaultAddress);
+  const currentVaultDetails = useCurrentVaultDetails(vaultAddress);
+
+  const totalSupply = currentVaultDetails?.totalSupply.toLocaleString() ?? "";
+
+  const tokenLogos = useMemo(() => {
+    if (!currentVault) return [];
+
+    const logos = [
+      getTokenLogo(currentVault.rewardTokenADetail),
+      currentVault.isDualFactory && currentVault.rewardTokenBDetail
+        ? getTokenLogo(currentVault.rewardTokenBDetail)
+        : undefined,
+    ].filter(Boolean);
+
+    return logos;
+  }, [currentVault]);
+
+  const dailyRewards = useMemo(() => {
+    if (!currentVault) return [];
+
+    const rewards = [
+      formatReward(
+        currentVault.dailyEmissionRewardA,
+        currentVault.rewardTokenADetail
+      ),
+      currentVault.isDualFactory && currentVault.rewardTokenBDetail
+        ? formatReward(
+            currentVault.dailyEmissionRewardB,
+            currentVault.rewardTokenBDetail
+          )
+        : null,
+    ]
+      .filter(
+        (reward): reward is string => reward !== null && reward !== undefined
+      )
+      .map((reward, index) => {
+        const [amount, symbol] = reward.split(" ");
+        return { amount, symbol, logo: tokenLogos[index] };
+      });
+
+    return rewards;
+  }, [currentVault, tokenLogos]);
 
   return (
     <Flex direction={"column"} gap={"16px"}>
@@ -24,22 +79,20 @@ const InfoSection: React.FC = () => {
           gap={{ initial: "8px", sm: "20px", lg: "8px" }}
         >
           {vaultId &&
-            VAULTS_TOKEN_LOGOS[vaultId as keyof typeof VAULTS_TOKEN_LOGOS].map(
-              (tokenLogo, idx) => (
-                <Flex justify={"between"} align={"center"} width={"100%"}>
-                  <Flex gap={"8px"} align={"center"}>
-                    <img
-                      src={tokenLogo}
-                      alt={"token logo"}
-                      width={"36px"}
-                      height={"36px"}
-                    />
-                    <Text size={"1"}>{vaultId.split("-")[idx]}</Text>
-                  </Flex>
-                  <Text size={"6"}>17,123,456</Text>
+            dailyRewards.map((reward) => (
+              <Flex justify={"between"} align={"center"} width={"100%"}>
+                <Flex gap={"8px"} align={"center"}>
+                  <img
+                    src={reward.logo}
+                    alt={"token logo"}
+                    width={"36px"}
+                    height={"36px"}
+                  />
+                  <Text size={"1"}>{reward.symbol}</Text>
                 </Flex>
-              )
-            )}
+                <Text size={"6"}>{reward.amount}</Text>
+              </Flex>
+            ))}
         </Flex>
       </RewardBox>
 
@@ -49,7 +102,7 @@ const InfoSection: React.FC = () => {
       >
         <InfoBox>
           <TextLabel>TVL</TextLabel>
-          <TextValue>$250M</TextValue>
+          <TextValue>{totalSupply} OVL</TextValue>
         </InfoBox>
         <InfoBox>
           <TextLabel>APY</TextLabel>
