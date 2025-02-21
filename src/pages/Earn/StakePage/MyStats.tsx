f7a49f1c5982032cf8261d2fc0174e03c5aef643
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-02-21 11:43:47 +0200

    Integrate dynamic userStats data in MyStats section

--- src/pages/Earn/StakePage/MyStats.tsx
+++ src/pages/Earn/StakePage/MyStats.tsx
@@ -6,8 +6,39 @@ import {
   StatValue,
 } from "./my-stats-styles";
 import theme from "../../../theme";
+import { useVaultsState } from "../../../state/vaults/hooks";
+import { useMemo } from "react";
+import { useParams } from "react-router-dom";
+import {
+  useCurrentVault,
+  useCurrentVaultDetails,
+} from "../hooks/useCurrentVaultData";
+import { getVaultAddressByVaultName } from "../utils/currentVaultdata";
+import { formatReward } from "../utils/formatReward";
 
 const MyStats: React.FC = () => {
+  const { vaultId } = useParams();
+  const { userStats } = useVaultsState();
+  const vaultAddress = getVaultAddressByVaultName(vaultId);
+  const currentVault = useCurrentVault(vaultAddress);
+  const currentVaultDetails = useCurrentVaultDetails(vaultAddress);
+
+  const pendingRewards = useMemo(() => {
+    if (!currentVaultDetails?.userRewards) return [];
+    if (!currentVault) return [];
+
+    const { userRewards } = currentVaultDetails;
+
+    const rewards = [
+      formatReward(userRewards.rewardA, currentVault.rewardTokenADetail),
+      currentVault.isDualFactory && currentVault.rewardTokenBDetail
+        ? formatReward(userRewards.rewardB, currentVault.rewardTokenBDetail)
+        : null,
+    ].filter(Boolean);
+
+    return rewards;
+  }, [currentVaultDetails, currentVault]);
+
   return (
     <Flex direction={"column"}>
       <Text style={{ color: theme.color.grey10 }}>MY STATS</Text>
@@ -15,7 +46,7 @@ const MyStats: React.FC = () => {
       <MyStatsContainer>
         <StatCard>
           <Text size={"1"}>Current Balance</Text>
-          <StatValue>500 OVL</StatValue>
+          <StatValue>{userStats?.currentBalance} OVL</StatValue>
         </StatCard>
 
         <StatCard>
@@ -35,7 +66,14 @@ const MyStats: React.FC = () => {
             </ClaimRewardsButton>
           </Flex>
 
-          <StatValue>7,500,000 OVL + 7,500,000 BERA</StatValue>
+          <Flex gap={"10px"}>
+            {pendingRewards.map((reward, idx) => (
+              <StatValue key={idx}>
+                {idx === 1 && `+ `}
+                {reward}
+              </StatValue>
+            ))}
+          </Flex>
         </StatCard>
       </MyStatsContainer>
     </Flex>
