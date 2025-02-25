a34c942ff1ccf7e2c54f3d58ff44c667de44b411
AndresImach <andyci435@gmail.com>
2025-02-25 11:43:42 -0300

    adding cards image (test)

--- src/pages/PowerCards/PowerCardsGrid/PowerCard/index.tsx
+++ src/pages/PowerCards/PowerCardsGrid/PowerCard/index.tsx
@@ -1,15 +1,11 @@
-import { PowerCardContainer, CardButton } from "./power-card-styles";
+import { PowerCardContainer } from "./power-card-styles";
+
 import zengarden from "../../../../assets/images/power-cards/zengarden.png";
 
 export function PowerCard() {
   return (
     <PowerCardContainer>
-      <div className="card-side front">
-        <img src={zengarden} alt="Example" />
-      </div>
-      <div className="card-side back">
-        <CardButton>Burn Card</CardButton>
-      </div>
+      <img src={zengarden} alt="Example" />
     </PowerCardContainer>
   );
 }
