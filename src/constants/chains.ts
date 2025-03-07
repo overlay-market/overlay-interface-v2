df1393470feda7affe3afe2cf63a38cc00f81922
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-03-07 17:23:55 +0200

    Update BartioLogo in constants/chains

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,5 +1,5 @@
 import { Chain } from "viem";
-import BartioLogo from "../assets/images/bartio-logo.png";
+import BartioLogo from "../assets/images/bartio-logo.webp";
 
 export enum SUPPORTED_CHAINID {
   BERACHAIN = 80094,
