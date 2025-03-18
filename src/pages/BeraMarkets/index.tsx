998976b5fd22a06d542da543d0a386edae3f87a2
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-03-18 16:16:52 +0200

    Implement BeraMarkets page

---
+++ src/pages/BeraMarkets/index.tsx
@@ -0,0 +1,115 @@
+import { Flex, Text } from "@radix-ui/themes";
+import React, { useEffect, useMemo, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
+import theme from "../../theme";
+import {
+  BeraMarketsContainer,
+  BeraMarketsContent,
+  BeraMarketsWrapper,
+  LineSeparator,
+  Title,
+} from "./bera-markets-styles";
+import { TransformedMarketData } from "overlay-sdk";
+import useSDK from "../../providers/SDKProvider/useSDK";
+import MarketCards from "../../components/MarketCards";
+import { formatPriceWithCurrency } from "../../utils/formatPriceWithCurrency";
+
+const BeraMarkets: React.FC = () => {
+  const navigate = useNavigate();
+
+  const sdk = useSDK();
+
+  const [marketsData, setMarketsData] = useState<TransformedMarketData[]>([]);
+
+  useEffect(() => {
+    if (!sdk || !sdk.markets) return;
+
+    const fetchData = async () => {
+      try {
+        sdk.markets.transformMarketsData().then((activeMarkets) => {
+          activeMarkets && setMarketsData(activeMarkets);
+          console.log({ activeMarkets });
+        });
+      } catch (error) {
+        console.error("Error fetching markets:", error);
+      }
+    };
+
+    fetchData();
+  }, [sdk]);
+
+  const mockMarkets = useMemo(() => {
+    if (marketsData.length >= 4) return marketsData;
+
+    const baseMarket = marketsData[0] || {
+      marketId: "mock-market-1",
+      price: 1000,
+      priceCurrency: "USD",
+    };
+
+    return [...marketsData, ...Array(4 - marketsData.length).fill(baseMarket)];
+  }, [marketsData]);
+
+  const redirectToMarketsPage = () => {
+    navigate(`/markets`);
+  };
+
+  return (
+    <BeraMarketsWrapper>
+      <Flex
+        justify={"start"}
+        align={"center"}
+        gap={"8px"}
+        width={"100%"}
+        height={{ initial: "47px", sm: theme.headerSize.height }}
+        px={"16px"}
+        py={"12px"}
+      >
+        <Flex
+          align={"center"}
+          gap={"4px"}
+          px={"4px"}
+          py={"7px"}
+          style={{ color: theme.color.blue3, cursor: "pointer" }}
+          onClick={() => redirectToMarketsPage()}
+        >
+          <ArrowLeftIcon />
+          <Text size={"1"} weight={"medium"}>
+            Back
+          </Text>
+        </Flex>
+
+        <Text weight={"medium"}>Bera Markets</Text>
+      </Flex>
+      <LineSeparator />
+
+      <BeraMarketsContainer>
+        <BeraMarketsContent>
+          <Title>Berachain Markets</Title>
+
+          <Flex gap={{ initial: "12px", lg: "16px" }} wrap={"wrap"}>
+            {mockMarkets &&
+              mockMarkets.map((market, index) => (
+                <MarketCards
+                  id={market?.marketId}
+                  key={index}
+                  priceWithCurrency={formatPriceWithCurrency(
+                    market.price ?? 0,
+                    market.priceCurrency,
+                    Number(market.price) > 10000 &&
+                      Number(market.price) < 1000000
+                      ? 5
+                      : 4
+                  )}
+                  title={decodeURIComponent(market.marketId)}
+                />
+              ))}
+          </Flex>
+        </BeraMarketsContent>
+      </BeraMarketsContainer>
+    </BeraMarketsWrapper>
+  );
+};
+
+export default BeraMarkets;
