8c231158faf2977346f4c3c6b17e56c19f3465d2
AndresImach <andyci435@gmail.com>
2025-03-28 12:44:49 -0300

    Improve PowerCardsGrid to handle wallet connection and enhance empty state messaging

--- src/pages/PowerCards/PowerCardsGrid/index.tsx
+++ src/pages/PowerCards/PowerCardsGrid/index.tsx
@@ -3,6 +3,7 @@ import { EmptyState } from "../power-cards-styles";
 import { AvailableCard, BurntCard, OwnedCard } from "./PowerCard";
 import { Container } from "./power-cards-grid-styles";
 import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import useAccount from "../../../hooks/useAccount";
 
 interface PowerCardsGridProps {
   activeTab: number;
@@ -22,66 +23,76 @@ export function PowerCardsGrid({
   setSelectedCard,
 }: PowerCardsGridProps) {
   const userData = userCardsData?.account?.erc1155Tokens || [];
+  const { address: account } = useAccount();
 
   return (
     <Container>
-      {activeTab === 0 &&
-        allCards.map((card: UnifiedCardData) => (
-          <AvailableCard
-            key={card.id}
-            card={card}
-            setSelectedCard={setSelectedCard}
-          />
-        ))}
-
-      {activeTab === 1 &&
-        userData.map((card: UnifiedCardData) => (
-          <OwnedCard
-            key={card.id}
-            card={card}
-            setSelectedCard={setSelectedCard}
-          />
-        ))}
-
-      {activeTab === 2 &&
-        userData?.map((card: UnifiedCardData) => (
-          <BurntCard key={card.id} card={card} />
-        ))}
-
-      {!allCards?.length && activeTab === 0 && (
-        <EmptyState>No Power Cards available</EmptyState>
-      )}
-
-      {userData.length === 0 && activeTab === 1 && (
-        <EmptyState>
-          No cards to display
-          <a
-            href="https://testnets.opensea.io/es/collection/powercard"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              display: "inline-flex",
-              alignItems: "center",
-              gap: "4px",
-              background: "linear-gradient(90deg, #ffc955 0%, #ff7cd5 100%)",
-              WebkitBackgroundClip: "text",
-              WebkitTextFillColor: "transparent",
-              textDecoration: "underline",
-              cursor: "pointer",
-            }}
-          >
-            See the full collection on OpenSea{" "}
-            <ExternalLinkIcon
-              style={{
-                color: "#ff7cd5",
-              }}
+      {activeTab === 0 && (
+        <>
+          {allCards.map((card: UnifiedCardData) => (
+            <AvailableCard
+              key={card.id}
+              card={card}
+              setSelectedCard={setSelectedCard}
             />
-          </a>
-        </EmptyState>
+          ))}
+          {!allCards?.length && (
+            <EmptyState>No Power Cards available</EmptyState>
+          )}
+        </>
       )}
 
-      {allCards?.length === 0 && activeTab === 2 && (
-        <EmptyState>No burnt Power Cards found</EmptyState>
+      {activeTab !== 0 && !account ? (
+        <EmptyState>Please connect your wallet to view Power Cards</EmptyState>
+      ) : (
+        <>
+          {activeTab === 1 &&
+            userData.map((card: UnifiedCardData) => (
+              <OwnedCard
+                key={card.id}
+                card={card}
+                setSelectedCard={setSelectedCard}
+              />
+            ))}
+
+          {activeTab === 2 &&
+            userData?.map((card: UnifiedCardData) => (
+              <BurntCard key={card.id} card={card} />
+            ))}
+
+          {userData.length === 0 && activeTab === 1 && (
+            <EmptyState>
+              No cards to display
+              <a
+                href="https://testnets.opensea.io/es/collection/powercard"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{
+                  display: "inline-flex",
+                  alignItems: "center",
+                  gap: "4px",
+                  background:
+                    "linear-gradient(90deg, #ffc955 0%, #ff7cd5 100%)",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                  textDecoration: "underline",
+                  cursor: "pointer",
+                }}
+              >
+                See the full collection on OpenSea{" "}
+                <ExternalLinkIcon
+                  style={{
+                    color: "#ff7cd5",
+                  }}
+                />
+              </a>
+            </EmptyState>
+          )}
+
+          {account && userData?.length === 0 && activeTab === 2 && (
+            <EmptyState>No burnt Power Cards found</EmptyState>
+          )}
+        </>
       )}
     </Container>
   );
