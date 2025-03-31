e7c5ce88da7d53e870e3e35ec90e29ba3d6fa65b
AndresImach <andyci435@gmail.com>
2025-03-31 11:00:58 -0300

    Center align PowerCardsGrid container for improved layout

--- src/pages/PowerCards/PowerCardsGrid/power-cards-grid-styles.tsx
+++ src/pages/PowerCards/PowerCardsGrid/power-cards-grid-styles.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
+  align-self: center;
   grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
   gap: 24px;
   padding: 16px 0;
