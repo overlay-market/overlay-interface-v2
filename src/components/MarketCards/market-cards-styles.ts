feb644989303b7c601b87159705371651a933b60
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-04-03 13:23:55 +0300

    Add mobilewidth prop to MarketCards component

--- src/components/MarketCards/market-cards-styles.ts
+++ src/components/MarketCards/market-cards-styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import theme from "../../theme";
 import { Flex } from "@radix-ui/themes";
 
-export const CustomCard = styled(Flex)`
+export const CustomCard = styled(Flex)<{mobilewidth?: string}>`
   aspect-ratio: 200 / 257;
   width: 100%;
   max-width: 200px;
@@ -18,7 +18,7 @@ export const CustomCard = styled(Flex)`
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 
   @media (max-width: 480px) {
-    max-width: 150px;
+    max-width: ${(props) => (props.mobilewidth ? props.mobilewidth : '150px')};
   }
 `;
 
