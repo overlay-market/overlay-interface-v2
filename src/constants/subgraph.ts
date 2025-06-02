b72ce41726c7ad6ca5a4621418367b1a4c33430a
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-06-02 14:59:56 +0300

    Update BSC testnet subgraph url

--- src/constants/subgraph.ts
+++ src/constants/subgraph.ts
@@ -4,7 +4,7 @@ export const CHAIN_SUBGRAPH_URL: Record<number, string> = {
   // [SUPPORTED_CHAINID.MAINNET]: 'https://api.thegraph.com/subgraphs/name/bigboydiamonds/overlay-v1-subgraph',
   [SUPPORTED_CHAINID.MAINNET]: 'https://api.studio.thegraph.com/query/46086/overlay-subgraph-eth/v2.0.2',
   [SUPPORTED_CHAINID.ARBITRUM_SEPOLIA]:
-  'https://gateway-arbitrum.network.thegraph.com/api/84ab887f7372fde61d4f1e228fa25964/subgraphs/id/59rgxaXFUQv5K6UXb1JJhS44uyPYn2EJFEZrZJLG5a4Y',
+    'https://gateway-arbitrum.network.thegraph.com/api/84ab887f7372fde61d4f1e228fa25964/subgraphs/id/59rgxaXFUQv5K6UXb1JJhS44uyPYn2EJFEZrZJLG5a4Y',
   [SUPPORTED_CHAINID.GÖRLI]: 'https://api.thegraph.com/subgraphs/name/bigboydiamonds/overlay-v1-subgraph-goerli',
   [SUPPORTED_CHAINID.RINKEBY]: 'https://api.thegraph.com/subgraphs/name/bigboydiamonds/overlay-v1-subgraph-rinkeby',
   // [SUPPORTED_CHAINID.ARBITRUM]: 'https://api.thegraph.com/subgraphs/name/bigboydiamonds/overlay-v1-subgraph-arbitrum',
