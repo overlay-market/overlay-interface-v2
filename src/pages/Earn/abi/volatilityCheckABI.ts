38ec151650b109e5e986bfd3f5b113b95d5fcce3
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-07-04 13:02:51 +0300

    Implement useVolatility hook

---
+++ src/pages/Earn/abi/volatilityCheckABI.ts
@@ -0,0 +1,21 @@
+export const volatilityCheckABI = [
+  {
+    "inputs":[
+      {
+        "internalType":"address",
+        "name":"vault",
+        "type":"address"
+      }
+    ],
+    "name":"currentVolatility",
+    "outputs":[
+      {
+        "internalType":"uint256",
+        "name":"volatility",
+        "type":"uint256"
+      }
+    ],
+    "stateMutability":"view",
+    "type":"function"
+  }
+] as const;
