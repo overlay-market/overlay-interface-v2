26f20e96b64db0ec8c01008fe5ea789612c19443
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-07-21 17:39:01 +0300

    Add TokenDisplay component to display selected token state

---
+++ src/pages/Trade/TradeWidget/ChainAndTokenSelect/TokenDisplay.tsx
@@ -0,0 +1,66 @@
+import { Avatar, Flex, Skeleton, Text } from "@radix-ui/themes";
+import theme from "../../../../theme";
+import { useTradeState } from "../../../../state/trade/hooks";
+import { SelectState } from "../../../../types/selectChainAndTokenTypes";
+import { DEFAULT_TOKEN } from "../../../../constants/applications";
+import { useAccount } from "wagmi";
+
+interface TokenDisplayProps {
+  onClick: () => void;
+}
+
+const TokenDisplay: React.FC<TokenDisplayProps> = ({ onClick }) => {
+  const { tokenState, selectedToken } = useTradeState();
+  const { address: account } = useAccount();
+
+  return (
+    <Flex
+      justify="center"
+      align="center"
+      gap="1"
+      style={{ cursor: "pointer" }}
+      onClick={
+        account && tokenState !== SelectState.LOADING ? onClick : undefined
+      }
+    >
+      {tokenState === SelectState.LOADING && (
+        <>
+          <Skeleton
+            width="40px"
+            height="40px"
+            style={{ borderRadius: "50%" }}
+          />
+          <Text>
+            <Skeleton style={{ borderRadius: "6px" }}>Select token</Skeleton>
+          </Text>
+        </>
+      )}
+      {tokenState === SelectState.EMPTY && (
+        <>
+          <Avatar radius="full" fallback="" />
+          <Text size="2" weight={"medium"} style={{ color: theme.color.grey3 }}>
+            Select token...
+          </Text>
+        </>
+      )}
+      {tokenState === SelectState.DEFAULT && (
+        <>
+          <Avatar radius="full" fallback="" src={DEFAULT_TOKEN.logoURI} />
+          <Text size="2" weight={"medium"} style={{ color: theme.color.blue1 }}>
+            {DEFAULT_TOKEN.symbol}
+          </Text>
+        </>
+      )}
+      {tokenState === SelectState.SELECTED && (
+        <>
+          <Avatar radius="full" fallback="" src={selectedToken.logoURI} />
+          <Text size="2" weight={"medium"} style={{ color: theme.color.blue1 }}>
+            {selectedToken.symbol}
+          </Text>
+        </>
+      )}
+    </Flex>
+  );
+};
+
+export default TokenDisplay;
