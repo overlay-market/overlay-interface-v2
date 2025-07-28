a2b98075268c97146d3214ec124098075080f73b
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-07-28 20:17:32 +0300

    Update TokenBalance component to use useOvlTokenBalance hook

--- src/components/Wallet/TokenBalance.tsx
+++ src/components/Wallet/TokenBalance.tsx
@@ -1,35 +1,11 @@
 import { Text } from "@radix-ui/themes";
-import { SUPPORTED_CHAINID } from "../../constants/chains";
 import Loader from "../Loader";
 import NumberSpring from "../NumberSpring";
 import theme from "../../theme";
-import { useEffect, useState } from "react";
-import useSDK from "../../providers/SDKProvider/useSDK";
-import useMultichainContext from "../../providers/MultichainContextProvider/useMultichainContext";
-import useAccount from "../../hooks/useAccount";
-import { useIsNewTxnHash } from "../../state/trade/hooks";
+import { useOvlTokenBalance } from "../../hooks/useOvlTokenBalance";
 
 const TokenBalance: React.FC = () => {
-  const sdk = useSDK();
-  const { chainId } = useMultichainContext();
-  const { address: account } = useAccount();
-  const isNewTxnHash = useIsNewTxnHash();
-  const [ovlBalance, setOvlBalance] = useState<number | undefined>(undefined);
-
-  useEffect(() => {
-    const fetchBalance = async () => {
-      if (account) {
-        try {
-          const ovlBalance = await sdk.ovl.balance(account, 8);
-          ovlBalance && setOvlBalance(Number(ovlBalance));
-        } catch (error) {
-          console.error("Error fetching ovlBalance:", error);
-        }
-      }
-    };
-
-    fetchBalance();
-  }, [chainId, account, sdk, isNewTxnHash]);
+  const { ovlBalance, isLoading, isError } = useOvlTokenBalance();
 
   return (
     <Text
@@ -41,10 +17,8 @@ const TokenBalance: React.FC = () => {
         opacity: 0.8,
       }}
     >
-      {ovlBalance === undefined ? (
+      {ovlBalance === undefined || isLoading || isError ? (
         <Loader size="12px" stroke="white" />
-      ) : SUPPORTED_CHAINID.MAINNET === chainId ? (
-        <NumberSpring inputValue={ovlBalance} text="OVL" />
       ) : (
         <NumberSpring inputValue={ovlBalance} text="OVL" />
       )}
