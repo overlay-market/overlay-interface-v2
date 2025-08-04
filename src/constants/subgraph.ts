045cc7752898d880a90fa8165ccac0d1985a0e6f
AndresImach <andyci435@gmail.com>
2025-08-04 13:31:22 -0300

    Add BSC Mainnet support and update related references

--- src/constants/subgraph.ts
+++ src/constants/subgraph.ts
@@ -15,4 +15,5 @@ export const CHAIN_SUBGRAPH_URL: Record<number, string> = {
   [SUPPORTED_CHAINID.IMOLA]: 'https://subgraph.overlay.market/query/subgraphs/name/overlay/v1-subgraph',
   [SUPPORTED_CHAINID.BARTIO]: 'https://api.goldsky.com/api/public/project_clyiptt06ifuv01ul9xiwfj28/subgraphs/overlay-bartio/prod/gn',
   [SUPPORTED_CHAINID.BSC_TESTNET]: 'https://api.goldsky.com/api/public/project_clyiptt06ifuv01ul9xiwfj28/subgraphs/overlay-bnb-testnet/latest/gn',
+  [SUPPORTED_CHAINID.BSC_MAINNET]: 'https://api.goldsky.com/api/public/project_clyiptt06ifuv01ul9xiwfj28/subgraphs/overlay-bsc/prod/gn',
 }
