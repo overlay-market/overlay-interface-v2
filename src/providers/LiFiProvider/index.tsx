3d1e16b24023318150d46669a0a4afff0f33d950
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-08-05 19:29:44 +0300

    Integrate LiFi sdk with EVM wallet client

--- src/providers/LiFiProvider/index.tsx
+++ src/providers/LiFiProvider/index.tsx
@@ -1,5 +1,7 @@
 import React, { createContext, useEffect, useState } from "react";
-import { createConfig, config as lifiConfig, SDKConfig } from "@lifi/sdk";
+import { createConfig, EVM, config as lifiConfig, SDKConfig } from "@lifi/sdk";
+import { useWalletClient } from "wagmi";
+import { createWalletClient, custom } from "viem";
 
 interface LiFiProviderProps {
   children: React.ReactNode;
@@ -10,15 +12,29 @@ export const LiFiContext = createContext<typeof lifiConfig | null>(null);
 
 export const LiFiProvider = ({ children, sdkConfig }: LiFiProviderProps) => {
   const [isInitialized, setIsInitialized] = useState(false);
+  const { data: walletClient } = useWalletClient();
 
   useEffect(() => {
+    if (!walletClient) return;
+
+    const client = createWalletClient({
+      account: walletClient.account,
+      chain: walletClient.chain,
+      transport: custom(walletClient.transport),
+    });
+
     createConfig({
       integrator: "overlay",
+      providers: [
+        EVM({
+          getWalletClient: async () => client,
+        }),
+      ],
       ...sdkConfig,
     });
 
     setIsInitialized(true);
-  }, [sdkConfig]);
+  }, [sdkConfig, walletClient]);
 
   if (!isInitialized) return null;
 
