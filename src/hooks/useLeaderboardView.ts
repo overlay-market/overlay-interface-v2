29c8fd1a3f26eb8465cbd66b463cda72c516a0b7
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-08-13 17:09:54 +0300

    Add formatted user and ranks data in useLeaderboardView

--- src/hooks/useLeaderboardView.ts
+++ src/hooks/useLeaderboardView.ts
@@ -1,6 +1,8 @@
 import { useMemo, useState } from "react";
 import useActiveMarkets from "./useActiveMarkets";
 import { ColumnKey, DisplayUserData, ExtendedUserData } from "../pages/Leaderboard/types";
+import { formatDecimalToPercentage } from "../utils/formatDecimal";
+import { formatPriceWithCurrency } from "../utils/formatPriceWithCurrency";
 
 export interface ColumnDef {
   value: ColumnKey;
@@ -13,16 +15,22 @@ type Return = {
   selectedLabel: string;
   columnOptions: { value: ColumnKey; label: string }[];
   formattedUserdata?: DisplayUserData;
+  formattedRanks?: DisplayUserData[];
 };
 
 type Params = {
   ranks?: ExtendedUserData[];
-  currentUserData?: ExtendedUserData;
+  currentUser?: ExtendedUserData;
 };
+
+function formatToSigDigits(value: number, digits: number) {
+  const str = Number(value).toPrecision(digits);
+  return Number(str).toString();
+}
 
 export const useLeaderboardView = ({
   ranks,
-  currentUserData,  
+  currentUser,  
 }: Params): Return => {
   const { data: markets } = useActiveMarkets();
 
@@ -44,16 +52,87 @@ export const useLeaderboardView = ({
   );
 
   const formattedUserdata: DisplayUserData | undefined = useMemo(() => {
-    if (!currentUserData) return undefined;
+    if (!currentUser) return undefined;
     const market = markets?.find(
-      m => m.id === currentUserData.mostTradedMarket.marketId
+      m => m.id === currentUser.mostTradedMarket.marketId
     );
+    const formattedWinRate =
+      Number(currentUser.winRate) > 0
+        ? `${formatDecimalToPercentage(currentUser.winRate)}%`
+        : "N/A";
+    
+    const tranformedTotalProfitOVL = Number(currentUser.totalProfitOVL) / 1e18;
+
+    const formattedTotalProfitOVL = formatToSigDigits(
+      tranformedTotalProfitOVL,
+      2
+    );  
+
+    const tranformedTotalProfitUSD = Number(currentUser.totalProfitUSD) / 1e18;
+
+    const formattedTotalProfitUSD = formatPriceWithCurrency(
+      tranformedTotalProfitUSD,
+      "$"
+    );  
+    
+    const transformedTotalVolumeOVL = Number(currentUser.totalVolumeOVL) / 1e18;
+    const transformedTotalFeesOVL = Number(currentUser.totalFeesOVL) / 1e18;
+
+
     return {
-      ...currentUserData,
+      ...currentUser,
+      winRate: formattedWinRate,
+      totalProfitOVL: formattedTotalProfitOVL,
+      totalProfitUSD: formattedTotalProfitUSD,
+      totalVolumeOVL: formatToSigDigits(transformedTotalVolumeOVL, 2),
+      totalFeesOVL: formatToSigDigits(transformedTotalFeesOVL, 2),
       marketId: market?.marketId || "",
       marketName: market?.marketName || "",
     };
-  }, [currentUserData, markets]);
+  }, [currentUser, markets]);
 
-  return { selectedColumn, setSelectedColumn, selectedLabel, columnOptions, formattedUserdata };
+  const formattedRanks: DisplayUserData[] | undefined = useMemo(() => {
+  if (!ranks) return undefined;
+
+  return ranks.map(rank => {
+    const market = markets?.find(
+      m => m.id === rank.mostTradedMarket.marketId
+    );
+
+    const formattedWinRate =
+      Number(rank.winRate) > 0
+        ? `${formatDecimalToPercentage(rank.winRate)}%`
+        : "N/A";
+    
+    const tranformedTotalProfitOVL = Number(rank.totalProfitOVL) / 1e18;
+
+    const formattedTotalProfitOVL = formatToSigDigits(
+      tranformedTotalProfitOVL,
+      2
+    );  
+
+    const tranformedTotalProfitUSD = Number(rank.totalProfitUSD) / 1e18;
+
+    const formattedTotalProfitUSD = formatPriceWithCurrency(
+      tranformedTotalProfitUSD,
+      "$"
+    );  
+    
+    const transformedTotalVolumeOVL = Number(rank.totalVolumeOVL) / 1e18;
+    const transformedTotalFeesOVL = Number(rank.totalFeesOVL) / 1e18;
+
+    return {
+      ...rank,
+      winRate: formattedWinRate,
+      totalProfitOVL: formattedTotalProfitOVL,
+      totalProfitUSD: formattedTotalProfitUSD,
+      totalVolumeOVL: formatToSigDigits(transformedTotalVolumeOVL, 2),
+      totalFeesOVL: formatToSigDigits(transformedTotalFeesOVL, 2),
+      marketId: market?.marketId || "",
+      marketName: market?.marketName || "",
+    };
+  });
+}, [ranks, markets]);
+
+  return { selectedColumn, setSelectedColumn, selectedLabel, columnOptions, formattedUserdata, formattedRanks };
 };
