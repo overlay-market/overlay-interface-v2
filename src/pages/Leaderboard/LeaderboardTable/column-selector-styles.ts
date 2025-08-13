492316069303cf4f28db2b85834dfe37cebb3afd
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-08-13 14:55:12 +0300

    Add ColumnSelector component for leaderboard column dropdown selection

---
+++ src/pages/Leaderboard/LeaderboardTable/column-selector-styles.ts
@@ -0,0 +1,43 @@
+import { Box, DropdownMenu } from "@radix-ui/themes";
+import styled from "styled-components";
+import theme from "../../../theme";
+
+export const ColumnSelectButton = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: end;  
+  gap: 12px;
+  background: none;
+  border: none;
+  outline: none;
+  padding: 0;
+  color: ${theme.color.grey2};
+  font-size: 12px;
+  cursor: pointer;
+`
+
+export const StyledDropdownContent = styled(DropdownMenu.Content)`
+  background: #191A26;
+  padding: 0px;
+  position: relative;
+  margin-right: -12px;
+  margin-top: 8px;
+  outline: none;
+  border-radius: 0px;
+  box-shadow: ${theme.color.grey7} 0px 4px 6px 0px;
+`;
+
+export const StyledDropdownItem = styled(DropdownMenu.Item)`
+  display: flex;
+  justify-content: flex-end; 
+  padding: 22px 12px;
+  color: ${theme.color.grey3};
+  border-radius: 4px;
+  cursor: pointer;
+  user-select: none;
+  border-bottom: 1px solid ${theme.color.grey7};
+  
+  &:hover {
+    background-color: ${theme.color.grey7};
+  }
+`;
