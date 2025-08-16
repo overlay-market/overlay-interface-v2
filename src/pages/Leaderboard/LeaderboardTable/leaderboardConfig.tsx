a19dd02b0a7c536e46f3dd3d7cbf2a2dfd26904a
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-08-16 04:11:17 +0300

    Update leaderboardConfig to use ranking by volume and comment out non-volume columns

--- src/pages/Leaderboard/LeaderboardTable/leaderboardConfig.tsx
+++ src/pages/Leaderboard/LeaderboardTable/leaderboardConfig.tsx
@@ -1,34 +1,34 @@
 import { ColumnDef, Ranking } from "../types";
-import MostTradedMarketLogo from "./MostTradedMarketLogo";
+// import MostTradedMarketLogo from "./MostTradedMarketLogo";
 
-export const RANKING_BY: Ranking = Ranking.ByProfit;
+export const RANKING_BY: Ranking = Ranking.ByVolume;
 
 export const leaderboardColumns: ColumnDef[] = [
-  {
-    value: "profitOVL",
-    label: "Profit OVL",
-    render: (data) => data.totalProfitOVL,
-  },
-  {
-    value: "profitUSD",
-    label: "Profit USD",
-    render: (data) => data.totalProfitUSD,
-  },
-  {
-    value: "positions",
-    label: "Number of Positions",
-    render: (data) => data.totalPositions,
-  },
-  {
-    value: "mostTradedMarket",
-    label: "Most Traded Market",
-    render: (data) => (
-      <MostTradedMarketLogo
-        marketId={data.marketId}
-        marketName={data.marketName}
-      />
-    ),
-  },
+  // {
+  //   value: "profitOVL",
+  //   label: "Profit OVL",
+  //   render: (data) => data.totalProfitOVL,
+  // },
+  // {
+  //   value: "profitUSD",
+  //   label: "Profit USD",
+  //   render: (data) => data.totalProfitUSD,
+  // },
+  // {
+  //   value: "positions",
+  //   label: "Number of Positions",
+  //   render: (data) => data.totalPositions,
+  // },
+  // {
+  //   value: "mostTradedMarket",
+  //   label: "Most Traded Market",
+  //   render: (data) => (
+  //     <MostTradedMarketLogo
+  //       marketId={data.marketId}
+  //       marketName={data.marketName}
+  //     />
+  //   ),
+  // },
   {
     value: "winRate",
     label: "Win Rate",
