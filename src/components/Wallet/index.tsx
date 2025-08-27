d5c311e40c1c64a35ac1d4d99bfcc198e844e27f
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-08-27 15:15:54 +0300

    Add Buy OVL button for connected wallets with navigation to Exchange page

--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -1,11 +1,20 @@
-import { Box, Flex } from "@radix-ui/themes";
-// import { GradientOutlineButton } from "../Button";
+import { useNavigate } from "react-router-dom";
 import theme from "../../theme";
+import { Box, Flex } from "@radix-ui/themes";
+import { GradientOutlineButton } from "../Button";
 import ChainSwitch from "./ChainSwitch";
 import ConnectWalletModal from "../ConnectWalletModal";
-// import HeaderMenu from "../HeaderMenu";
+import useAccount from "../../hooks/useAccount";
 
 const Wallet: React.FC = () => {
+  const { address } = useAccount();
+
+  const navigate = useNavigate();
+
+  const toggleWidget = () => {
+    navigate("/exchange");
+  };
+
   return (
     <Box
       width="308px"
@@ -19,23 +28,23 @@ const Wallet: React.FC = () => {
       style={{ zIndex: "10" }}
     >
       <Flex
-        gap={"20px"}
-        align={"center"}
-        height={"100%"}
-        justify={"end"}
+        gap="20px"
+        align="center"
+        height="100%"
+        justify="end"
         pr={{ initial: "15px", sm: "20px" }}
       >
-        {/* <GradientOutlineButton
-          title={"Buy OV"}
-          width={"78px"}
-          height={"29px"}
-          size={"12px"}
-          handleClick={() => {
-            console.log("buy OV!");
-          }}
-        /> */}
+        {address && (
+          <GradientOutlineButton
+            title="Buy OVL"
+            width="84px"
+            height="29px"
+            size="12px"
+            handleClick={toggleWidget}
+          />
+        )}
+
         <ChainSwitch />
-        {/* <HeaderMenu /> */}
         <ConnectWalletModal />
       </Flex>
     </Box>
