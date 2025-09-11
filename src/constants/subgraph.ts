07dc21e0b99a13b58d4c7df1f27b350276d53eb1
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-09-11 22:13:37 +0300

    add useReferralAccountData hook

--- src/constants/subgraph.ts
+++ src/constants/subgraph.ts
@@ -3,3 +3,5 @@ import { SUPPORTED_CHAINID } from "./chains";
 export const CHAIN_SUBGRAPH_URL: Record<number, string> = {
   [SUPPORTED_CHAINID.BSC_MAINNET]: 'https://api.goldsky.com/api/public/project_clyiptt06ifuv01ul9xiwfj28/subgraphs/overlay-bsc/prod/gn',
 }
+
+export const SUBGRAPH_WITH_REFERRAL_URL = 'https://api.goldsky.com/api/public/project_clyiptt06ifuv01ul9xiwfj28/subgraphs/overlay-bsc/4/gn'
