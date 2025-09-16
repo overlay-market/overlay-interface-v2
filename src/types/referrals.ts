d6bbf5ad7e0ab41775172bbee0a37126bb536c13
Aliona-D <alona.danyliuk@inventorsoft.co>
2025-09-16 18:36:19 +0300

    add useTokenTransfersReferralData hook

--- src/types/referrals.ts
+++ src/types/referrals.ts
@@ -20,3 +20,21 @@ export type ReferralAccountQuery = {
     }>;
   } | null;
 };
+
+export type TokenTransfersQueryVariables = {
+  to: string 
+  from: string 
+  token: string
+}
+
+export type TokenTransfersQuery = {
+  tokenTransfers: Array<{
+    amount: string
+    from: string
+    to: string
+    transaction: {
+      id: string
+      timestamp: number
+    }
+  }>
+}
