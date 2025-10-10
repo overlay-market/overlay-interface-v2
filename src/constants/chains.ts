6fc8378b0ef876c4a1cd477fa1df6a42d5d7c58c
Arthur G. <me@arthurka.eu>
2025-10-10 18:51:06 +0200

    wip: enable testnet

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,9 +1,10 @@
 import { Chain } from "viem";
-import { bsc } from "viem/chains";
+import { bsc, bscTestnet } from "viem/chains";
 import BscTestnetLogo from "../assets/images/bsc-testnet-logo.webp";
 
 export enum SUPPORTED_CHAINID {
-  BSC_MAINNET = 56
+  BSC_MAINNET = 56,
+  BSC_TESTNET = 97
 }
 
 export const DEFAULT_NET = "BSC";
@@ -11,23 +12,28 @@ export const DEFAULT_CHAINID: number = SUPPORTED_CHAINID.BSC_MAINNET;
 export const DEFAULT_CHAIN_LOGO = BscTestnetLogo;
 
 export const WORKING_CHAINS = [
-  SUPPORTED_CHAINID[SUPPORTED_CHAINID.BSC_MAINNET]
+  SUPPORTED_CHAINID[SUPPORTED_CHAINID.BSC_MAINNET],
+  SUPPORTED_CHAINID[SUPPORTED_CHAINID.BSC_TESTNET]
 ];
 
 export const VIEM_CHAINS: { [key: number]: Chain } = {
   [SUPPORTED_CHAINID.BSC_MAINNET]: bsc,
+  [SUPPORTED_CHAINID.BSC_TESTNET]: bscTestnet,
 };
 
 export const NETWORK_ICONS: {
   [chainId in SUPPORTED_CHAINID | number]: string;
 } = {
   [SUPPORTED_CHAINID.BSC_MAINNET]: BscTestnetLogo,
+  [SUPPORTED_CHAINID.BSC_TESTNET]: BscTestnetLogo,
 };
 
 export const CHAIN_LIST: { [chainId in SUPPORTED_CHAINID | number]: string } = {
-  [SUPPORTED_CHAINID.BSC_MAINNET]: "BSC"
+  [SUPPORTED_CHAINID.BSC_MAINNET]: "BSC",
+  [SUPPORTED_CHAINID.BSC_TESTNET]: "BSC Testnet"
 };
 
 export const CHAIN_LIST_ORDER: { [x: number]: number } = {
   [0]: SUPPORTED_CHAINID.BSC_MAINNET,
+  [1]: SUPPORTED_CHAINID.BSC_TESTNET,
 };
