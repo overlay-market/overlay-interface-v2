b60821b458378f375127e558f1f86414e4cd70ca
TomasCImach <tomas90@gmail.com>
2026-05-06 10:41:55 -0300

    chore: update bsc subgraph endpoint

--- src/constants/subgraph.ts
+++ src/constants/subgraph.ts
@@ -1,7 +1,7 @@
 import { SUPPORTED_CHAINID } from "./chains";
 
 export const CHAIN_SUBGRAPH_URL: Record<number, string> = {
-  [SUPPORTED_CHAINID.BSC_MAINNET]: 'https://api.goldsky.com/api/public/project_clyiptt06ifuv01ul9xiwfj28/subgraphs/overlay-bsc/9-lbsc-new-gamba/gn',
+  [SUPPORTED_CHAINID.BSC_MAINNET]: 'https://api.goldsky.com/api/public/project_clyiptt06ifuv01ul9xiwfj28/subgraphs/overlay-bsc/prod/gn',
   [SUPPORTED_CHAINID.BSC_TESTNET]: 'https://api.goldsky.com/api/public/project_clyiptt06ifuv01ul9xiwfj28/subgraphs/overlay-bnb-testnet/6-stable-pnl/gn',
 }
 
